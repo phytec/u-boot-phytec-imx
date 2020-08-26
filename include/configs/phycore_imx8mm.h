3f5c58e7cb015944830903db8cad14bc5495680f
Teresa Remmet <t.remmet@phytec.de>
2020-08-26 10:01:47 +0200

    include: configs: phycore_imx8mm: Use RAM size detection
    Remove RAM size from board include to activate RAM size detection.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- include/configs/phycore_imx8mm.h
+++ include/configs/phycore_imx8mm.h
@@ -100,7 +100,6 @@
 #define CONFIG_SYS_SDRAM_BASE		0x40000000
 
 #define PHYS_SDRAM			SZ_1G
-#define PHYS_SDRAM_SIZE                 SZ_2G /* 2GB DDR */
 
 /* UART */
 #define CONFIG_MXC_UART_BASE		UART3_BASE_ADDR
