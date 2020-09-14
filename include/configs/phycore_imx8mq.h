b8f3069d1594158b630a1a8f23c42460432cf41e
Janine Hagemann <j.hagemann@phytec.de>
2020-09-14 08:52:34 +0200

    include: configs: phycore_imx8mq: Remove PHYS_SDRAM_SIZE
    Remove PHYS_SDRAM_SIZE because we use the board files for different ram
    timings.
    
    Sigend-off-by: Janine Hagemann <j.hagemann@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- include/configs/phycore_imx8mq.h
+++ include/configs/phycore_imx8mq.h
@@ -127,7 +127,6 @@
 
 #define CONFIG_SYS_SDRAM_BASE           0x40000000
 #define PHYS_SDRAM                      0x40000000
-#define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
 
 #define CONFIG_MXC_UART_BASE		UART1_BASE_ADDR
 
