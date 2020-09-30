5e5813494e324045af1a50a7322012f84483a7f6
Teresa Remmet <t.remmet@phytec.de>
2020-09-30 14:17:56 +0200

    board: phycore-imx8mm: Switch back to non-DM SPL
    To save around 50kB of space which is needed for more features we
    switch the SPL back to non-DM support.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Reviewed-by: Christian Hemp <c.hemp@phytec.de>

--- include/configs/phycore_imx8mm.h
+++ include/configs/phycore_imx8mm.h
@@ -27,9 +27,12 @@
 #define CONFIG_SYS_SPL_MALLOC_SIZE	SZ_512K
 
 /* malloc f used before GD_FLG_FULL_MALLOC_INIT set */
-#define CONFIG_MALLOC_F_ADDR		0x930000
+#define CONFIG_MALLOC_F_ADDR		0x912000
 /* For RAW image gives a error info not panic */
 #define CONFIG_SPL_ABORT_ON_RAW_IMAGE
+
+#define CONFIG_SYS_I2C
+
 #endif
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
