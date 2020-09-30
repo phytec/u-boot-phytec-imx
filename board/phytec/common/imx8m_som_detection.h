66be289e7f08fb015c760d47637c8162dbed28fb
Teresa Remmet <t.remmet@phytec.de>
2020-09-30 14:17:52 +0200

    board: phytec: common: Move imx8m ddr detection to common
    The RAM setup can be read out of i2c EEPROM populated on PHYTEC
    i.MX8M/i.MX8MM/i.MX8MN/i.MX8MP modules. The EEPROM layout fits to all
    those boards. So move this to common place.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Reviewed-by: Christian Hemp <c.hemp@phytec.de>

---
+++ board/phytec/common/imx8m_som_detection.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2020 PHYTEC Messtechnik GmbH
+ * Author: Teresa Remmet <t.remmet@phytec.de>
+ */
+
+#ifndef _PHYTEC_IMX8M_DDR_H
+#define _PHYTEC_IMX8M_DDR_H
+
+int get_imx8m_ddr_size(char *of_path);
+
+#endif
