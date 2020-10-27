49c01f28956978e580dfc411d7cf5d660c7ba518
Teresa Remmet <t.remmet@phytec.de>
2020-10-27 14:49:03 +0100

    board: phytec: Initial phyCORE-i.MX8MP support
    Add initial support for phyCORE-i.MX8MP.
    Not all freatures are tested yet:
    - debug UART
    - SD-card
    - eMMC
    - SPI-NOR
    - PMIC
    - i2c EEPROM
    - ethernet
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

---
+++ board/phytec/phycore_imx8mp/phycore-imx8mp.c
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2020 PHYTEC Messtechnik GmbH
+ * Author: Teresa Remmet <t.remmet@phytec.de>
+ */
+
+#include <common.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/io.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <env.h>
+#include <miiphy.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+static int setup_fec(void)
+{
+	struct iomuxc_gpr_base_regs *gpr =
+		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+
+	/* Enable RGMII TX clk output */
+	setbits_le32(&gpr->gpr[1], BIT(22));
+
+	return 0;
+}
+
+int board_init(void)
+{
+	setup_fec();
+
+	return 0;
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return devno;
+}
+
+int board_late_init(void)
+{
+	switch (get_boot_device()) {
+	case SD2_BOOT:
+		env_set_ulong("mmcdev", 1);
+		break;
+	case MMC3_BOOT:
+		env_set_ulong("mmcdev", 2);
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
