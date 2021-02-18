63b4df7522caa0cca905c599946740dcfe3237d9
Teresa Remmet <t.remmet@phytec.de>
2021-02-18 11:15:49 +0100

    board: phycore-imx8mp: Change debug UART interface
    Due to power domain changes within the new HW revision 1549.1a
    we had to change the debug UART from 2 to 1.
    
    Keep backwards compatibility for the first alpha HW rev for now.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/phycore_imx8mp/spl.c
+++ board/phytec/phycore_imx8mp/spl.c
@@ -185,9 +185,14 @@ int board_fit_config_name_match(const char *name)
 #define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
 #define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
 
-static iomux_v3_cfg_t const uart_pads[] = {
+static iomux_v3_cfg_t const uart2_pads[] = {
 	MX8MP_PAD_UART2_RXD__UART2_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
 	MX8MP_PAD_UART2_TXD__UART2_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const uart1_pads[] = {
+	MX8MP_PAD_UART1_RXD__UART1_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX8MP_PAD_UART1_TXD__UART1_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
 };
 
 static iomux_v3_cfg_t const wdog_pads[] = {
@@ -202,9 +207,13 @@ int board_early_init_f(void)
 
 	set_wdog_reset(wdog);
 
-	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
-
-	init_uart_clk(1);
+	if (config_enabled(CONFIG_TARGET_PHYCORE_IMX8MP_REV0)) {
+		imx_iomux_v3_setup_multiple_pads(uart2_pads, ARRAY_SIZE(uart2_pads));
+		init_uart_clk(1);
+	} else {
+		imx_iomux_v3_setup_multiple_pads(uart1_pads, ARRAY_SIZE(uart1_pads));
+		init_uart_clk(0);
+	}
 
 	return 0;
 }
