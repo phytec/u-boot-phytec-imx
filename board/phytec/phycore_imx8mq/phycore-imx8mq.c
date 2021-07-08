90e2f7cce6fc73b3606078d676639e9842063c6a
Janine Hagemann <j.hagemann@phytec.de>
2021-07-08 11:15:48 +0000

    board: phytec: phycore-imx8mq: Add USB sdp support
    Adds USB sdp and fastboot support so the uuu tool can be
    used.
    
    Signed-off-by: Janine Hagemann <j.hagemann@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/phycore_imx8mq/phycore-imx8mq.c
+++ board/phytec/phycore_imx8mq/phycore-imx8mq.c
@@ -14,6 +14,9 @@
 #include <miiphy.h>
 #include <power/pfuze100_pmic.h>
 #include <power/pmic.h>
+#include <usb.h>
+#include <dwc3-uboot.h>
+
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -61,6 +64,80 @@ int board_phy_config(struct phy_device *phydev)
 	return 0;
 }
 
+#define USB_PHY_CTRL0                  0xF0040
+#define USB_PHY_CTRL0_REF_SSP_EN       BIT(2)
+
+#define USB_PHY_CTRL1                  0xF0044
+#define USB_PHY_CTRL1_RESET            BIT(0)
+#define USB_PHY_CTRL1_COMMONONN                BIT(1)
+#define USB_PHY_CTRL1_ATERESET         BIT(3)
+#define USB_PHY_CTRL1_VDATSRCENB0      BIT(19)
+#define USB_PHY_CTRL1_VDATDETENB0      BIT(20)
+
+#define USB_PHY_CTRL2                  0xF0048
+#define USB_PHY_CTRL2_TXENABLEN0       BIT(8)
+
+static struct dwc3_device dwc3_device_data = {
+	.maximum_speed = USB_SPEED_SUPER,
+	.base = USB1_BASE_ADDR,
+	.dr_mode = USB_DR_MODE_PERIPHERAL,
+	.index = 0,
+	.power_down_scale = 2,
+};
+
+int usb_gadget_handle_interrupts(void)
+{
+	dwc3_uboot_handle_interrupt(0);
+	return 0;
+}
+
+static void dwc3_nxp_usb_phy_init(struct dwc3_device *dwc3)
+{
+	u32 RegData;
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL1);
+	RegData &= ~(USB_PHY_CTRL1_VDATSRCENB0 | USB_PHY_CTRL1_VDATDETENB0 |
+			USB_PHY_CTRL1_COMMONONN);
+	RegData |= USB_PHY_CTRL1_RESET | USB_PHY_CTRL1_ATERESET;
+	writel(RegData, dwc3->base + USB_PHY_CTRL1);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL0);
+	RegData |= USB_PHY_CTRL0_REF_SSP_EN;
+	writel(RegData, dwc3->base + USB_PHY_CTRL0);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL2);
+	RegData |= USB_PHY_CTRL2_TXENABLEN0;
+	writel(RegData, dwc3->base + USB_PHY_CTRL2);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL1);
+	RegData &= ~(USB_PHY_CTRL1_RESET | USB_PHY_CTRL1_ATERESET);
+	writel(RegData, dwc3->base + USB_PHY_CTRL1);
+}
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	int ret = 0;
+
+	if (index == 0 && init == USB_INIT_DEVICE) {
+		imx8m_usb_power(index, true);
+		dwc3_nxp_usb_phy_init(&dwc3_device_data);
+		return dwc3_uboot_init(&dwc3_device_data);
+	} else if (index == 0 && init == USB_INIT_HOST)
+		return ret;
+
+	return 0;
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	int ret = 0;
+	if (index == 0 && init == USB_INIT_DEVICE) {
+		dwc3_uboot_exit(index);
+		imx8m_usb_power(index, false);
+	}
+	return ret;
+}
+
 void imx8mq_set_bootsource(void)
 {
 	if (get_boot_device() == MMC1_BOOT) {
@@ -75,6 +152,8 @@ void imx8mq_set_bootsource(void)
 int board_init(void)
 {
 	setup_fec();
+	init_usb_clk();
+
 	return 0;
 }
 
@@ -90,4 +169,9 @@ int board_late_init(void)
 	env_set("board_rev", "iMX8MQ");
 
 	return 0;
+}
+
+int mmc_map_to_kernel_blk(int dev_no)
+{
+	return dev_no;
 }
