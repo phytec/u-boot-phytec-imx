71c2e091436caab88eef98e9b2da8531a880c2d1
Janine Hagemann <j.hagemann@phytec.de>
2021-10-08 16:17:41 +0200

    imx: mx8m: add imx8m_ddrc_sdram_size
    Add a function to set the SDRAM-size automatically for
    i.MX8MQ and i.MX8MM.
    Also expand this patch for i.MX8MM.
    Based on barebox commit: 3da52f308a60 ("ARM: i.MX: esdctl:
    Add memory size detection for i.MX8MQ").
    
    Signed-off-by: Janine Hagemann <j.hagemann@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    [Updated patch for v2020.04 and moved calculation to mmdc_size.c]
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    [cherry picked from v2020.04_2.3.2-phy]
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- arch/arm/mach-imx/mmdc_size.c
+++ arch/arm/mach-imx/mmdc_size.c
@@ -2,6 +2,8 @@
 
 #include <common.h>
 #include <asm/io.h>
+#include <linux/bitfield.h>
+#include <asm/arch/sys_proto.h>
 
 #if defined(CONFIG_MX53)
 #define MEMCTL_BASE	ESDCTL_BASE_ADDR
@@ -12,6 +14,44 @@
 #endif
 static const unsigned char col_lookup[] = {9, 10, 11, 8, 12, 9, 9, 9};
 static const unsigned char bank_lookup[] = {3, 2};
+
+#if defined(CONFIG_IMX8M)
+#define DDRC_ADDRMAP(n)                                (0x200 + 4 * (n))
+#define DDRC_ADDRMAP6_LPDDR4_6GB_12GB_24GB     GENMASK(30, 29)
+#define DDRC_ADDRMAP6_LPDDR3_6GB_12GB          BIT(31)
+#define DDRC_ADDRMAP0_CS_BIT0                  GENMASK(4, 0)
+
+#define DDRC_MSTR                              0x0000
+#define DDRC_MSTR_LPDDR4                       BIT(5)
+#define DDRC_MSTR_DATA_BUS_WIDTH               GENMASK(13, 12)
+#define DDRC_MSTR_ACTIVE_RANKS                 GENMASK(27, 24)
+
+#define DDRC_ADDRMAP0_CS_BIT1                  GENMASK(12,  8)
+
+#define DDRC_ADDRMAP1_BANK_B2                  GENMASK(20, 16)
+
+#define DDRC_ADDRMAP2_COL_B5                   GENMASK(27, 24)
+#define DDRC_ADDRMAP2_COL_B4                   GENMASK(19, 16)
+
+#define DDRC_ADDRMAP3_COL_B9                   GENMASK(27, 24)
+#define DDRC_ADDRMAP3_COL_B8                   GENMASK(19, 16)
+#define DDRC_ADDRMAP3_COL_B7                   GENMASK(11,  8)
+#define DDRC_ADDRMAP3_COL_B6                   GENMASK(3,  0)
+#define DDRC_ADDRMAP4_COL_B10                  GENMASK(3, 0)
+#define DDRC_ADDRMAP4_COL_B11                  GENMASK(11, 8)
+
+#define DDRC_ADDRMAP5_ROW_B11                  GENMASK(27, 24)
+
+#define DDRC_ADDRMAP6_ROW_B15                  GENMASK(27, 24)
+#define DDRC_ADDRMAP6_ROW_B14                  GENMASK(19, 16)
+#define DDRC_ADDRMAP6_ROW_B13                  GENMASK(11,  8)
+#define DDRC_ADDRMAP6_ROW_B12                  GENMASK(3,  0)
+
+#define DDRC_ADDRMAP7_ROW_B17                  GENMASK(11,  8)
+#define DDRC_ADDRMAP7_ROW_B16                  GENMASK(3,  0)
+
+#define MEMCTL_BASE    0x3d400000
+#endif
 
 /* these MMDC registers are common to the IMX53 and IMX6 */
 struct esd_mmdc_regs {
@@ -54,4 +94,119 @@ unsigned int imx_ddr_size(void)
 		return 0xf0000000;
 
 	return 1 << bits;
+}
+
+unsigned int imx_ddrc_count_bits(unsigned int bits, const u8 config[],
+				 unsigned int config_num)
+{
+	for (int i = 0; i < config_num && config[i] == 0b1111; i++)
+		bits--;
+
+	return bits;
+}
+
+resource_size_t imx_ddrc_sdram_size(void *ddrc, const u32 addrmap[],
+				    u8 col_max, const u8 col_b[],
+				    unsigned int col_b_num,
+				    u8 row_max, const u8 row_b[],
+				    unsigned int row_b_num,
+				    bool reduced_address_space)
+{
+	const u32 mstr = readl(ddrc + DDRC_MSTR);
+	unsigned int banks, ranks, columns, rows, active_ranks, width;
+	resource_size_t size;
+
+	if (is_imx8mn())
+		banks = 1;
+	else
+		banks = 2;
+	ranks = 0;
+
+	switch (FIELD_GET(DDRC_MSTR_ACTIVE_RANKS, mstr)) {
+	case 0b0001:
+		active_ranks = 1;
+		break;
+	case 0b0011:
+		active_ranks = 2;
+		break;
+	case 0b1111:
+		active_ranks = 4;
+		break;
+	default:
+		BUG();
+	}
+
+	switch (FIELD_GET(DDRC_MSTR_DATA_BUS_WIDTH, mstr)) {
+	case 0b00:	/* Full DQ bus  */
+		width = 4;
+		break;
+	case 0b01:	/* Half DQ bus  */
+		width = 2;
+		break;
+	case 0b10:	/* Quarter DQ bus  */
+		width = 1;
+		break;
+	default:
+		BUG();
+	}
+
+	if (active_ranks == 4 &&
+	    (FIELD_GET(DDRC_ADDRMAP0_CS_BIT1, addrmap[0]) != 0b11111))
+			ranks++;
+	if (active_ranks > 1 &&
+	    (FIELD_GET(DDRC_ADDRMAP0_CS_BIT0, addrmap[0]) != 0b11111))
+			ranks++;
+
+	if (FIELD_GET(DDRC_ADDRMAP1_BANK_B2, addrmap[1]) != 0b11111)
+		banks++;
+
+	columns	= imx_ddrc_count_bits(col_max, col_b, col_b_num);
+	rows	= imx_ddrc_count_bits(row_max, row_b, row_b_num);
+
+	size = ((u64)(1 << banks) * width << (rows + columns)) << ranks;
+
+	return reduced_address_space ? size * 3 / 4 : size;
+}
+
+resource_size_t imx8m_ddrc_sdram_size(void)
+{
+	void __iomem *mem_base = (void __iomem *)MEMCTL_BASE;
+
+	const u32 addrmap[] = {
+		readl(mem_base + DDRC_ADDRMAP(0)),
+		readl(mem_base + DDRC_ADDRMAP(1)),
+		readl(mem_base + DDRC_ADDRMAP(2)),
+		readl(mem_base + DDRC_ADDRMAP(3)),
+		readl(mem_base + DDRC_ADDRMAP(4)),
+		readl(mem_base + DDRC_ADDRMAP(5)),
+		readl(mem_base + DDRC_ADDRMAP(6)),
+		readl(mem_base + DDRC_ADDRMAP(7))
+	};
+
+	const u8 col_b[] = {
+		FIELD_GET(DDRC_ADDRMAP4_COL_B11, addrmap[4]),
+		FIELD_GET(DDRC_ADDRMAP4_COL_B10, addrmap[4]),
+		FIELD_GET(DDRC_ADDRMAP3_COL_B9,  addrmap[3]),
+		FIELD_GET(DDRC_ADDRMAP3_COL_B8,  addrmap[3]),
+		FIELD_GET(DDRC_ADDRMAP3_COL_B7,  addrmap[3]),
+		FIELD_GET(DDRC_ADDRMAP3_COL_B6,  addrmap[3]),
+		FIELD_GET(DDRC_ADDRMAP2_COL_B5,  addrmap[2]),
+		FIELD_GET(DDRC_ADDRMAP2_COL_B4,  addrmap[2]),
+	};
+
+	const u8 row_b[] = {
+		FIELD_GET(DDRC_ADDRMAP6_ROW_B15, addrmap[6]),
+		FIELD_GET(DDRC_ADDRMAP6_ROW_B14, addrmap[6]),
+		FIELD_GET(DDRC_ADDRMAP6_ROW_B13, addrmap[6]),
+		FIELD_GET(DDRC_ADDRMAP6_ROW_B12, addrmap[6]),
+		FIELD_GET(DDRC_ADDRMAP5_ROW_B11, addrmap[5]),
+	};
+
+	const bool reduced_address_space =
+		FIELD_GET(DDRC_ADDRMAP6_LPDDR4_6GB_12GB_24GB, addrmap[6]);
+
+	return imx_ddrc_sdram_size(mem_base, addrmap,
+					12, col_b, sizeof(col_b),
+					16, row_b, sizeof(row_b),
+					reduced_address_space);
 }
