663adc8e87652d4bbf5d3a3b3909dd44a5bcebd1
Teresa Remmet <t.remmet@phytec.de>
2022-05-04 17:03:57 +0200

    board: phytec: phycore-imx8mp: Enable more USB features
    Enables support for USB Host and flashing over UUU.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/phycore_imx8mp/phycore-imx8mp.c
+++ board/phytec/phycore_imx8mp/phycore-imx8mp.c
@@ -5,15 +5,18 @@
  */
 
 #include <common.h>
+#include <asm/arch/clock.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/global_data.h>
 #include <asm/io.h>
 #include <asm/mach-imx/boot_mode.h>
+#include <dwc3-uboot.h>
 #include <env.h>
 #include <fdt_support.h>
 #include <jffs2/load_kernel.h>
 #include <miiphy.h>
 #include <mtd_node.h>
+#include <usb.h>
 
 #include "../common/imx8m_som_detection.h"
 
@@ -21,6 +24,18 @@ DECLARE_GLOBAL_DATA_PTR;
 
 #define EEPROM_ADDR		0x51
 #define EEPROM_ADDR_FALLBACK	0x59
+
+static struct dwc3_device dwc3_device_data = {
+#ifdef CONFIG_SPL_BUILD
+	.maximum_speed = USB_SPEED_HIGH,
+#else
+	.maximum_speed = USB_SPEED_SUPER,
+#endif
+	.base = USB1_BASE_ADDR,
+	.dr_mode = USB_DR_MODE_PERIPHERAL,
+	.index = 0,
+	.power_down_scale = 2,
+};
 
 int ft_board_setup(void *blob,struct bd_info *bd)
 {
@@ -49,11 +64,40 @@ static int setup_fec(void)
 	return 0;
 }
 
+int board_usb_init(int index, enum usb_init_type init)
+{
+	imx8m_usb_power(index, true);
+
+	if (index == 0 && init == USB_INIT_DEVICE) {
+		return dwc3_uboot_init(&dwc3_device_data);
+	}
+
+	return 0;
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	if (index == 0 && init == USB_INIT_DEVICE)
+		dwc3_uboot_exit(index);
+
+	imx8m_usb_power(index, false);
+
+	return 0;
+}
+
+int usb_gadget_handle_interrupts(int index)
+{
+	dwc3_uboot_handle_interrupt(index);
+	return 0;
+}
+
 int board_init(void)
 {
 	phytec_eeprom_data_setup(0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
 
 	setup_fec();
+
+	init_usb_clk();
 
 	return 0;
 }
@@ -61,6 +105,11 @@ int board_init(void)
 int board_mmc_get_env_dev(int devno)
 {
 	return devno;
+}
+
+int mmc_map_to_kernel_blk(int dev_no)
+{
+        return dev_no;
 }
 
 int board_late_init(void)
