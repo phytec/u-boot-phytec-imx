a2c1d3135b897c9a35939e00392b2c45571f9c9b
Teresa Remmet <t.remmet@phytec.de>
2022-06-07 11:58:44 +0200

    arm: imx: mmdc_size: fix LPDDR4 size calculation
    The DDRC only uses the DEVICE_CONFIG field for memory types other than
    LPDDR4. While LPDDR4 always has a bus width of x32, the script aid
    generates the value for a x16 bus, as this was apparently used for the
    controller validation. This resulted in the calculated DRAM size to be
    halved on boards with LPDDR4 memory.
    
    This patch is ported from barebox and written by:
    Lucas Stach <l.stach@pengutronix.de>
    commit 1e7d4ace64cc (ARM: imx: esdctl: fix LPDDR4 size calculation)
    Link: https://lore.barebox.org/20220513141625.1411217-1-l.stach@pengutronix.de
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- arch/arm/mach-imx/mmdc_size.c
+++ arch/arm/mach-imx/mmdc_size.c
@@ -148,7 +148,7 @@ resource_size_t imx_ddrc_sdram_size(void *ddrc, const u32 addrmap[DDRC_ADDRMAP_L
 	}
 
 	/* Bus width in bytes, 0 means half byte or 4-bit mode */
-	if (is_imx8)
+	if (is_imx8 && !(mstr & DDRC_MSTR_LPDDR4))
 		width = (1 << FIELD_GET(DDRC_MSTR_DEVICE_CONFIG, mstr)) >> 1;
 	else
 		width = 4;
