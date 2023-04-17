phytec/u-boot-phytec-imx

commit 7abca023ec45c174c320e6b13c793d99cd04d42f

boards: phytec: common: Split SOM detection
Split the SOM detection implementation into a common module
for basic functionality and a further one which provides
an API for all i.MX8 SOCs.

This rework allows to add more SOCs from different vendors
alongside the current i.MX8 implementation.

Update all API calls in the imx8mm, imx8mn, and imx8mp board
files.

Signed-off-by: Daniel Schultz <d.schultz@phytec.de>
Reviewed-by: Teresa Remmet <t.remmet@phytec.de>
Signed-off-by: Teresa Remmet <t.remmet@phytec.de>

board/phytec/common/Makefile
@@ -7,4 +7,5 @@ ifdef CONFIG_SPL_BUILD
 obj- := __dummy__.o
 endif
 
-obj-y += imx8m_som_detection.o
+obj-$(CONFIG_PHYTEC_SOM_DETECTION) += phytec_som_detection.o
+obj-$(CONFIG_PHYTEC_IMX8M_SOM_DETECTION) += imx8m_som_detection.o

board/phytec/common/imx8m_som_detection.c
@@ -14,103 +14,18 @@
 
 #include "imx8m_som_detection.h"
 
-struct phytec_eeprom_data eeprom_data;
+extern struct phytec_eeprom_data eeprom_data;
 
-int _phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
-			      int bus_num, int addr, int addr_fallback)
+/* Check if the SoM is actually one of the following products:
+ * - i.MX8MM
+ * - i.MX8MN
+ * - i.MX8MP
+ * - i.MX8MQ
+ *
+ * Returns 0 in case it's a known SoM. Otherwise, returns -1.
+ */
+u8 phytec_imx8m_detect(u8 som, char *opt)
 {
-	int ret;
-
-	if (!IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION))
-		return -ENOSYS;
-
-	ret = _phytec_eeprom_data_init(data, bus_num, addr);
-	if (ret) {
-		pr_err("%s: init failed. Trying fall back address 0x%x\n",
-		       __func__, addr_fallback);
-		ret = _phytec_eeprom_data_init(data, bus_num, addr_fallback);
-	}
-
-	if (ret)
-		pr_err("%s: EEPROM data init failed\n", __func__);
-	else
-		pr_info("%s: init successful\n", __func__);
-
-	return ret;
-}
-
-int _phytec_eeprom_data_init(struct phytec_eeprom_data *data,
-			     int bus_num, int addr)
-{
-	int ret, i;
-	unsigned int crc;
-	u8 som;
-	char *opt;
-	int *ptr;
-
-	if (!data)
-		data = &eeprom_data;
-
-#if CONFIG_IS_ENABLED(DM_I2C)
-	struct udevice *dev;
-
-	ret = i2c_get_chip_for_busnum(bus_num, addr, 2, &dev);
-	if (ret) {
-		pr_err("%s: i2c EEPROM not found: %i.\n", __func__, ret);
-		return ret;
-	}
-
-	ret = dm_i2c_read(dev, 0, (uint8_t *)data,
-			  sizeof(struct phytec_eeprom_data));
-#else
-	i2c_set_bus_num(bus_num);
-	ret = i2c_read(addr, 0, 2, (uint8_t *)data,
-		       sizeof(struct phytec_eeprom_data));
-#endif
-	if (ret) {
-		pr_err("%s: Unable to read EEPROM data\n", __func__);
-		return ret;
-	}
-
-	if (data->api_rev == 0xff) {
-		pr_err("%s: EEPROM is not flashed. Prototype?\n", __func__);
-		return -EINVAL;
-	}
-
-	ptr = (int *)data;
-	for (i = 0; i < sizeof(struct phytec_eeprom_data); i += sizeof(ptr))
-		if (*ptr != 0x0)
-			break;
-
-	if (i == sizeof(struct phytec_eeprom_data)) {
-		pr_err("%s: EEPROM data is all zero. Erased?\n", __func__);
-		return -EINVAL;
-	}
-
-	if (data->api_rev > PHYTEC_API_REV2) {
-		pr_err("%s: EEPROM API revision %u not supported\n",
-			__func__, data->api_rev);
-		return -EINVAL;
-	}
-
-	/* We are done here for early revisions */
-	if (data->api_rev <= PHYTEC_API_REV1)
-		return 0;
-
-	crc = crc8(0, (const unsigned char *)data,
-		   sizeof(struct phytec_eeprom_data));
-	debug("%s: crc: %x\n", __func__, crc);
-
-	if (crc) {
-		pr_err("%s: CRC mismatch. EEPROM data is not usable\n",
-		       __func__);
-		return -EINVAL;
-	}
-
-	som = data->data.data_api2.som_no;
-	debug("%s: som id: %u\n", __func__, som);
-	opt = _phytec_get_imx8m_opt(data);
-
 	if (som == PHYTEC_IMX8MP_SOM && is_imx8mp())
 		return 0;
 
@@ -121,55 +36,28 @@ int _phytec_eeprom_data_init(struct phytec_eeprom_data *data,
 		else if (((opt[0] - '0') == 0) &&
 			 ((opt[1] - '0') != 0) && is_imx8mn())
 			return 0;
-		goto err;
+		return -1;
 	}
 
 	if (som == PHYTEC_IMX8MQ_SOM && is_imx8mq())
 		return 0;
-err:
-	pr_err("%s: SoM ID does not match. Wrong EEPROM data?\n", __func__);
-	return -EINVAL;
-}
 
-char * __maybe_unused _phytec_get_imx8m_opt(struct phytec_eeprom_data *data)
-{
-	char *opt;
-
-	if (!data)
-		data = &eeprom_data;
-
-	switch (data->api_rev) {
-	case PHYTEC_API_REV0:
-	case PHYTEC_API_REV1:
-		opt = data->data.data_api0.opt;
-		break;
-	case PHYTEC_API_REV2:
-		opt = data->data.data_api2.opt;
-		break;
-	default:
-		opt = NULL;
-		break;
-	};
-
-	return opt;
+	return -1;
 }
 
 /*
  * So far all PHYTEC i.MX8M boards have RAM size definition at the
  * same location.
  */
-u8 __maybe_unused _phytec_get_imx8m_ddr_size(struct phytec_eeprom_data *data)
+u8 __maybe_unused phytec_get_imx8m_ddr_size(struct phytec_eeprom_data *data)
 {
 	char *opt;
 	u8 ddr_id;
 
-	if (!IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION))
-		return PHYTEC_EEPROM_INVAL;
-
 	if (!data)
 		data = &eeprom_data;
 
-	opt = _phytec_get_imx8m_opt(data);
+	opt = phytec_get_opt(data);
 	if (opt)
 		ddr_id = opt[2] - '0';
 	else
@@ -185,13 +73,10 @@ u8 __maybe_unused _phytec_get_imx8m_ddr_size(struct phytec_eeprom_data *data)
  * returns: 0x0 if no SPI is poulated. Otherwise a board depended
  * code for the size. PHYTEC_EEPROM_INVAL when the data is invalid.
  */
-u8 __maybe_unused _phytec_get_imx8m_spi(struct phytec_eeprom_data *data)
+u8 __maybe_unused phytec_get_imx8m_spi(struct phytec_eeprom_data *data)
 {
 	char *opt;
 	u8 spi;
-
-	if (!IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION))
-		return PHYTEC_EEPROM_INVAL;
 
 	if (!data)
 		data = &eeprom_data;
@@ -199,7 +84,7 @@ u8 __maybe_unused _phytec_get_imx8m_spi(struct phytec_eeprom_data *data)
 	if (data->api_rev < PHYTEC_API_REV2)
 		return PHYTEC_EEPROM_INVAL;
 
-	opt = _phytec_get_imx8m_opt(data);
+	opt = phytec_get_opt(data);
 	if (opt)
 		spi = opt[4] - '0';
 	else
@@ -215,13 +100,10 @@ u8 __maybe_unused _phytec_get_imx8m_spi(struct phytec_eeprom_data *data)
  * returns: 0x0 if no ethernet phy is poulated. 0x1 if it is populated.
  * PHYTEC_EEPROM_INVAL when the data is invalid.
  */
-u8 __maybe_unused _phytec_get_imx8m_eth(struct phytec_eeprom_data *data)
+u8 __maybe_unused phytec_get_imx8m_eth(struct phytec_eeprom_data *data)
 {
 	char *opt;
 	u8 eth;
-
-	if (!IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION))
-		return PHYTEC_EEPROM_INVAL;
 
 	if (!data)
 		data = &eeprom_data;
@@ -229,7 +111,7 @@ u8 __maybe_unused _phytec_get_imx8m_eth(struct phytec_eeprom_data *data)
 	if (data->api_rev < PHYTEC_API_REV2)
 		return PHYTEC_EEPROM_INVAL;
 
-	opt = _phytec_get_imx8m_opt(data);
+	opt = phytec_get_opt(data);
 	if (opt) {
 		eth = opt[5] - '0';
 		eth &= 0x1;
@@ -246,7 +128,7 @@ u8 __maybe_unused _phytec_get_imx8m_eth(struct phytec_eeprom_data *data)
  * returns: 0 if no RTC is poulated. 1 if it is populated.
  * PHYTEC_EEPROM_INVAL when the data is invalid.
  */
-u8 __maybe_unused _phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data)
+u8 __maybe_unused phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data)
 {
 	char *opt;
 	u8 rtc;
@@ -257,7 +139,7 @@ u8 __maybe_unused _phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data)
 	if (data->api_rev < PHYTEC_API_REV2)
 		return PHYTEC_EEPROM_INVAL;
 
-	opt = _phytec_get_imx8m_opt(data);
+	opt = phytec_get_opt(data);
 	if (opt) {
 		rtc = opt[5] - '0';
 		rtc &= 0x4;
@@ -267,70 +149,4 @@ u8 __maybe_unused _phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data)
 	}
 	debug("%s: rtc: %u\n", __func__, rtc);
 	return rtc;
-}
-
-void __maybe_unused _phytec_print_som_info(struct phytec_eeprom_data *data)
-{
-	struct phytec_api2_data *api2;
-	char pcb_sub_rev;
-	unsigned int ksp_no, sub_som_type1, sub_som_type2;
-
-	if (!IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION))
-		return;
-
-	if (!data)
-		data = &eeprom_data;
-
-	if (data->api_rev < PHYTEC_API_REV2)
-		return;
-
-	api2 = &data->data.data_api2;
-
-	/* Calculate PCB subrevision */
-	pcb_sub_rev = api2->pcb_sub_opt_rev & 0x0f;
-	pcb_sub_rev = pcb_sub_rev ? ((pcb_sub_rev - 1) + 'a') : 0;
-
-	/* print standard product string */
-	if (api2->som_type <= 1) {
-		printf("SoM: %s-%03u-%s.%s PCB rev: %u%c\n",
-		       phytec_som_type_str[api2->som_type], api2->som_no,
-		       api2->opt, api2->bom_rev, api2->pcb_rev, pcb_sub_rev);
-		return;
-	}
-	/* print KSP/KSM string */
-	if (api2->som_type <= 3) {
-		ksp_no = (api2->ksp_no << 8) | api2->som_no;
-		printf("SoM: %s-%u ",
-		       phytec_som_type_str[api2->som_type], ksp_no);
-	/* print standard product based KSP/KSM strings */
-	} else {
-		switch(api2->som_type) {
-		case 4:
-			sub_som_type1 = 0;
-			sub_som_type2 = 3;
-			break;
-		case 5:
-			sub_som_type1 = 0;
-			sub_som_type2 = 2;
-			break;
-		case 6:
-			sub_som_type1 = 1;
-			sub_som_type2 = 3;
-			break;
-		case 7:
-			sub_som_type1 = 1;
-			sub_som_type2 = 2;
-			break;
-		default:
-			break;
-		};
-
-		printf("SoM: %s-%03u-%s-%03u ",
-		       phytec_som_type_str[sub_som_type1],
-		       api2->som_no, phytec_som_type_str[sub_som_type2],
-		       api2->ksp_no);
-	}
-
-	printf("Option: %s BOM rev: %s PCB rev: %u%c\n", api2->opt,
-	       api2->bom_rev, api2->pcb_rev, pcb_sub_rev);
 }

board/phytec/common/imx8m_som_detection.h
@@ -4,81 +4,43 @@
  * Author: Teresa Remmet <t.remmet@phytec.de>
  */
 
-#ifndef _PHYTEC_IMX8M_DDR_H
-#define _PHYTEC_IMX8M_DDR_H
+#ifndef _PHYTEC_IMX8M_SOM_DETECTION_H
+#define _PHYTEC_IMX8M_SOM_DETECTION_H
 
-#define PHYTEC_MAX_OPTIONS	17
-#define PHYTEC_IMX8MQ_SOM	66
-#define PHYTEC_IMX8MM_SOM	69
-#define PHYTEC_IMX8MP_SOM	70
+#include "phytec_som_detection.h"
 
-#define PHYTEC_EEPROM_INVAL	0xff
+#if IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION)
 
-enum {
-	PHYTEC_API_REV0 = 0,
-	PHYTEC_API_REV1,
-	PHYTEC_API_REV2,
-};
+u8 phytec_imx8m_detect(u8 som, char *opt);
+u8 __maybe_unused phytec_get_imx8m_ddr_size(struct phytec_eeprom_data *data);
+u8 __maybe_unused phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data);
+u8 __maybe_unused phytec_get_imx8m_spi(struct phytec_eeprom_data *data);
+u8 __maybe_unused phytec_get_imx8m_eth(struct phytec_eeprom_data *data);
 
-static const char * const phytec_som_type_str[] = {
-	"PCM",
-	"PCL",
-	"KSM",
-	"KSP",
-};
+#else
 
-struct phytec_api0_data {
-	u8 pcb_rev;		/* PCB revision of SoM */
-	u8 som_type;		/* SoM type */
-	u8 ksp_no;		/* KSP no */
-	char opt[16];		/* SoM options */
-	u8 mac[6];		/* MAC address (optional) */
-	u8 pad[5];		/* padding */
-	u8 cksum;		/* checksum */
-} __attribute__ ((__packed__));
+inline u8 __maybe_unused phytec_get_imx8m_ddr_size(
+		struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
 
-struct phytec_api2_data {
-	u8 pcb_rev;		/* PCB revision of SoM */
-	u8 pcb_sub_opt_rev;	/* PCB subrevision and opt revision */
-	u8 som_type;		/* SoM type */
-	u8 som_no;		/* SoM number */
-	u8 ksp_no;		/* KSP information */
-	char opt[PHYTEC_MAX_OPTIONS]; /* SoM options */
-	char bom_rev[2];	/* BOM revision */
-	u8 mac[6];		/* MAC address (optional) */
-	u8 crc8;		/* checksum */
-} __attribute__ ((__packed__));
+inline u8 __maybe_unused phytec_get_imx8mp_rtc(
+		struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
 
-struct phytec_eeprom_data {
-	u8 api_rev;
-	union {
-		struct phytec_api0_data data_api0;
-		struct phytec_api2_data data_api2;
-	} data;
-} __attribute__ ((__packed__));
+inline u8 __maybe_unused phytec_get_imx8m_spi(struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
 
-int _phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
-			      int bus_num, int addr, int addr_fallback);
-int _phytec_eeprom_data_init(struct phytec_eeprom_data *data,
-			     int bus_num, int addr);
+inline u8 __maybe_unused phytec_get_imx8m_eth(struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
 
-char * __maybe_unused _phytec_get_imx8m_opt(struct phytec_eeprom_data *data);
-u8 __maybe_unused _phytec_get_imx8m_ddr_size(struct phytec_eeprom_data *data);
-u8 __maybe_unused _phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data);
-u8 __maybe_unused _phytec_get_imx8m_spi(struct phytec_eeprom_data *data);
-u8 __maybe_unused _phytec_get_imx8m_eth(struct phytec_eeprom_data *data);
-void __maybe_unused _phytec_print_som_info(struct phytec_eeprom_data *data);
+#endif /* IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION) */
 
-#define phytec_eeprom_data_setup(bus_num, addr, addr_fallback) \
-	(_phytec_eeprom_data_setup(NULL, bus_num, addr, addr_fallback))
-#define phytec_eeprom_data_init(bus_num, addr) \
-	(_phytec_eeprom_data_init(NULL, bus_num, addr))
-
-#define phytec_get_imx8m_opt() _phytec_get_imx8m_opt(NULL)
-#define phytec_get_imx8m_ddr_size() _phytec_get_imx8m_ddr_size(NULL)
-#define phytec_get_imx8mp_rtc() _phytec_get_imx8mp_rtc(NULL)
-#define phytec_get_imx8m_spi() _phytec_get_imx8m_spi(NULL)
-#define phytec_get_imx8m_eth() _phytec_get_imx8m_eth(NULL)
-#define phytec_print_som_info() _phytec_print_som_info(NULL)
-
-#endif
+#endif /* _PHYTEC_IMX8M_SOM_DETECTION_H */

board/phytec/common/phytec_som_detection.c
@@ -1,0 +1,209 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2020 PHYTEC Messtechnik GmbH
+ * Author: Teresa Remmet <t.remmet@phytec.de>
+ */
+
+#include <common.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <asm/arch/sys_proto.h>
+#include <dm/device.h>
+#include <dm/uclass.h>
+#include <i2c.h>
+#include <u-boot/crc.h>
+
+#include "phytec_som_detection.h"
+#include "imx8m_som_detection.h"
+
+struct phytec_eeprom_data eeprom_data;
+
+int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
+			     int bus_num, int addr, int addr_fallback)
+{
+	int ret;
+
+	ret = phytec_eeprom_data_init(data, bus_num, addr);
+	if (ret) {
+		pr_err("%s: init failed. Trying fall back address 0x%x\n",
+		       __func__, addr_fallback);
+		ret = phytec_eeprom_data_init(data, bus_num, addr_fallback);
+	}
+
+	if (ret)
+		pr_err("%s: EEPROM data init failed\n", __func__);
+	else
+		pr_info("%s: init successful\n", __func__);
+
+	return ret;
+}
+
+int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
+			    int bus_num, int addr)
+{
+	int ret, i;
+	unsigned int crc;
+	u8 som;
+	char *opt;
+	int *ptr;
+
+	if (!data)
+		data = &eeprom_data;
+
+#if CONFIG_IS_ENABLED(DM_I2C)
+	struct udevice *dev;
+
+	ret = i2c_get_chip_for_busnum(bus_num, addr, 2, &dev);
+	if (ret) {
+		pr_err("%s: i2c EEPROM not found: %i.\n", __func__, ret);
+		return ret;
+	}
+
+	ret = dm_i2c_read(dev, 0, (uint8_t *)data,
+			  sizeof(struct phytec_eeprom_data));
+#else
+	i2c_set_bus_num(bus_num);
+	ret = i2c_read(addr, 0, 2, (uint8_t *)data,
+		       sizeof(struct phytec_eeprom_data));
+#endif
+	if (ret) {
+		pr_err("%s: Unable to read EEPROM data\n", __func__);
+		return ret;
+	}
+
+	if (data->api_rev == 0xff) {
+		pr_err("%s: EEPROM is not flashed. Prototype?\n", __func__);
+		return -EINVAL;
+	}
+
+	ptr = (int *)data;
+	for (i = 0; i < sizeof(struct phytec_eeprom_data); i += sizeof(ptr))
+		if (*ptr != 0x0)
+			break;
+
+	if (i == sizeof(struct phytec_eeprom_data)) {
+		pr_err("%s: EEPROM data is all zero. Erased?\n", __func__);
+		return -EINVAL;
+	}
+
+	if (data->api_rev > PHYTEC_API_REV2) {
+		pr_err("%s: EEPROM API revision %u not supported\n",
+		       __func__, data->api_rev);
+		return -EINVAL;
+	}
+
+	/* We are done here for early revisions */
+	if (data->api_rev <= PHYTEC_API_REV1)
+		return 0;
+
+	crc = crc8(0, (const unsigned char *)data,
+		   sizeof(struct phytec_eeprom_data));
+	debug("%s: crc: %x\n", __func__, crc);
+
+	if (crc) {
+		pr_err("%s: CRC mismatch. EEPROM data is not usable\n",
+		       __func__);
+		return -EINVAL;
+	}
+
+	som = data->data.data_api2.som_no;
+	debug("%s: som id: %u\n", __func__, som);
+	opt = phytec_get_opt(data);
+	if (!opt)
+		return -EINVAL;
+
+	if (IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION))
+		ret = phytec_imx8m_detect(som, opt);
+
+	if (ret) {
+		pr_err("%s: SoM ID does not match. Wrong EEPROM data?\n", __func__);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+void __maybe_unused phytec_print_som_info(struct phytec_eeprom_data *data)
+{
+	struct phytec_api2_data *api2;
+	char pcb_sub_rev;
+	unsigned int ksp_no, sub_som_type1, sub_som_type2;
+
+	if (!data)
+		data = &eeprom_data;
+
+	if (data->api_rev < PHYTEC_API_REV2)
+		return;
+
+	api2 = &data->data.data_api2;
+
+	/* Calculate PCB subrevision */
+	pcb_sub_rev = api2->pcb_sub_opt_rev & 0x0f;
+	pcb_sub_rev = pcb_sub_rev ? ((pcb_sub_rev - 1) + 'a') : ' ';
+
+	/* print standard product string */
+	if (api2->som_type <= 1) {
+		printf("SoM: %s-%03u-%s.%s PCB rev: %u%c\n",
+		       phytec_som_type_str[api2->som_type], api2->som_no,
+		       api2->opt, api2->bom_rev, api2->pcb_rev, pcb_sub_rev);
+		return;
+	}
+	/* print KSP/KSM string */
+	if (api2->som_type <= 3) {
+		ksp_no = (api2->ksp_no << 8) | api2->som_no;
+		printf("SoM: %s-%u ",
+		       phytec_som_type_str[api2->som_type], ksp_no);
+	/* print standard product based KSP/KSM strings */
+	} else {
+		switch (api2->som_type) {
+		case 4:
+			sub_som_type1 = 0;
+			sub_som_type2 = 3;
+			break;
+		case 5:
+			sub_som_type1 = 0;
+			sub_som_type2 = 2;
+			break;
+		case 6:
+			sub_som_type1 = 1;
+			sub_som_type2 = 3;
+			break;
+		case 7:
+			sub_som_type1 = 1;
+			sub_som_type2 = 2;
+			break;
+		default:
+			break;
+		};
+
+		printf("SoM: %s-%03u-%s-%03u ",
+		       phytec_som_type_str[sub_som_type1],
+		       api2->som_no, phytec_som_type_str[sub_som_type2],
+		       api2->ksp_no);
+	}
+
+	printf("Option: %s BOM rev: %s PCB rev: %u%c\n", api2->opt,
+	       api2->bom_rev, api2->pcb_rev, pcb_sub_rev);
+}
+
+char * __maybe_unused phytec_get_opt(struct phytec_eeprom_data *data)
+{
+	char *opt;
+
+	if (!data)
+		data = &eeprom_data;
+
+	switch (data->api_rev) {
+	case PHYTEC_API_REV0:
+	case PHYTEC_API_REV1:
+		opt = data->data.data_api0.opt;
+		break;
+	case PHYTEC_API_REV2:
+		opt = data->data.data_api2.opt;
+		break;
+	default:
+		opt = NULL;
+		break;
+	};
+
+	return opt;
+}

board/phytec/common/phytec_som_detection.h
@@ -1,0 +1,96 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2020 PHYTEC Messtechnik GmbH
+ * Author: Teresa Remmet <t.remmet@phytec.de>
+ */
+
+#ifndef _PHYTEC_SOM_DETECTION_H
+#define _PHYTEC_SOM_DETECTION_H
+
+#define PHYTEC_MAX_OPTIONS	17
+#define PHYTEC_IMX8MQ_SOM	66
+#define PHYTEC_IMX8MM_SOM	69
+#define PHYTEC_IMX8MP_SOM	70
+
+#define PHYTEC_EEPROM_INVAL	0xff
+
+enum {
+	PHYTEC_API_REV0 = 0,
+	PHYTEC_API_REV1,
+	PHYTEC_API_REV2,
+};
+
+static const char * const phytec_som_type_str[] = {
+	"PCM",
+	"PCL",
+	"KSM",
+	"KSP",
+};
+
+struct phytec_api0_data {
+	u8 pcb_rev;		/* PCB revision of SoM */
+	u8 som_type;		/* SoM type */
+	u8 ksp_no;		/* KSP no */
+	char opt[16];		/* SoM options */
+	u8 mac[6];		/* MAC address (optional) */
+	u8 pad[5];		/* padding */
+	u8 cksum;		/* checksum */
+} __attribute__ ((__packed__));
+
+struct phytec_api2_data {
+	u8 pcb_rev;		/* PCB revision of SoM */
+	u8 pcb_sub_opt_rev;	/* PCB subrevision and opt revision */
+	u8 som_type;		/* SoM type */
+	u8 som_no;		/* SoM number */
+	u8 ksp_no;		/* KSP information */
+	char opt[PHYTEC_MAX_OPTIONS]; /* SoM options */
+	char bom_rev[2];	/* BOM revision */
+	u8 mac[6];		/* MAC address (optional) */
+	u8 crc8;		/* checksum */
+} __attribute__ ((__packed__));
+
+struct phytec_eeprom_data {
+	u8 api_rev;
+	union {
+		struct phytec_api0_data data_api0;
+		struct phytec_api2_data data_api2;
+	} data;
+} __attribute__ ((__packed__));
+
+#if IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION)
+
+int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
+			     int bus_num, int addr, int addr_fallback);
+int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
+			    int bus_num, int addr);
+void __maybe_unused phytec_print_som_info(struct phytec_eeprom_data *data);
+
+char * __maybe_unused phytec_get_opt(struct phytec_eeprom_data *data);
+
+# else
+
+inline int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
+				    int bus_num, int addr, int addr_fallback)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+inline int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
+				   int bus_num, int addr)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+inline void __maybe_unused phytec_print_som_info(
+		struct phytec_eeprom_data *data)
+{
+}
+
+inline char * __maybe_unused phytec_get_opt(struct phytec_eeprom_data *data)
+{
+	return NULL;
+}
+
+#endif /* IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION) */
+
+#endif /* _PHYTEC_SOM_DETECTION_H */

board/phytec/common/Kconfig
@@ -1,6 +1,12 @@
+config PHYTEC_SOM_DETECTION
+	bool "Support SoM detection for Phytec platforms"
+	help
+	   Support of I2C EEPROM based SoM detection.
+
 config PHYTEC_IMX8M_SOM_DETECTION
 	bool "Support SoM detection for i.MX8M Phytec platforms"
-	default y
+	depends on (TARGET_PHYCORE_IMX8MM || TARGET_PHYCORE_IMX8MN || TARGET_PHYCORE_IMX8MP)
+	select PHYTEC_SOM_DETECTION
 	help
 	   Support of I2C EEPROM based SoM detection. Supported
 	   for PHYTEC i.MX8M/i.MX8MM/i.MX8MN/i.MX8MP boards.

board/phytec/phycore_imx8mm/phycore-imx8mm.c
@@ -57,12 +57,12 @@ int board_fix_fdt(void *blob)
 	u8 opt;
 
 	/* We have no access to global variables here */
-	ret = _phytec_eeprom_data_setup(&data, 0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
+	ret = phytec_eeprom_data_setup(&data, 0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
 	if (ret)
 		return 0;
 
 	/* Disable fspi node if SPI flash if not populated */
-	opt = _phytec_get_imx8m_spi(&data);
+	opt = phytec_get_imx8m_spi(&data);
 	if (!opt) {
 		ret = fdt_disable_node(blob, "nxp,imx8mm-fspi");
 		if (ret)
@@ -71,7 +71,7 @@ int board_fix_fdt(void *blob)
 	}
 
 	/* Disable fec node if eth phy is not populated */
-	opt = _phytec_get_imx8m_eth(&data);
+	opt = phytec_get_imx8m_eth(&data);
 	if (!opt) {
 		ret = fdt_disable_node(blob, "fsl,imx8mm-fec");
 		if (ret)
@@ -85,7 +85,7 @@ int board_fix_fdt(void *blob)
 
 int ft_board_setup(void *blob, struct bd_info *bd)
 {
-	u8 spi = phytec_get_imx8m_spi();
+	u8 spi = phytec_get_imx8m_spi(NULL);
 	/* Do nothing if no SPI is poulated or data invalid */
 	if (spi == 0)
 		return 0;
@@ -144,7 +144,7 @@ static int setup_fec(void)
 
 int board_init(void)
 {
-	phytec_eeprom_data_setup(0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
+	phytec_eeprom_data_setup(NULL, 0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
 
 	setup_fec();
 
@@ -160,7 +160,7 @@ int board_late_init(void)
 {
 	u8 spi;
 
-	spi = phytec_get_imx8m_spi();
+	spi = phytec_get_imx8m_spi(NULL);
 
 	if (spi != 0 && spi != PHYTEC_EEPROM_INVAL)
 		env_set("spiprobe", "sf probe");
@@ -215,7 +215,7 @@ int extension_board_scan(struct list_head *extension_list)
 	int ret = 0;
 	u8 option;
 
-	option = phytec_get_imx8m_eth();
+	option = phytec_get_imx8m_eth(NULL);
 	if (!option) {
 		extension = add_extension("phyCORE-i.MX8MM no eth phy",
 					  "imx8mm-phycore-no-eth.dtbo",
@@ -233,7 +233,7 @@ int extension_board_scan(struct list_head *extension_list)
 		ret++;
 	}
 
-	option = phytec_get_imx8m_spi();
+	option = phytec_get_imx8m_spi(NULL);
 	if (!option) {
 		extension = add_extension("phyCORE-i.MX8MM no SPI flash",
 					  "imx8mm-phycore-no-spiflash.dtbo",

board/phytec/phycore_imx8mm/spl.c
@@ -56,13 +56,13 @@ static void spl_dram_init(void)
 	int ret;
 	int size = 0xf;
 
-	ret = phytec_eeprom_data_setup(0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
+	ret = phytec_eeprom_data_setup(NULL, 0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
 	if (ret && !IS_ENABLED(CONFIG_PHYCORE_IMX8MM_RAM_SIZE_FIX))
 		goto err;
 
 	if (!ret) {
 		printf("phytec_eeprom_data_init: init successful\n");
-		phytec_print_som_info();
+		phytec_print_som_info(NULL);
 	}
 
 	if (IS_ENABLED(CONFIG_PHYCORE_IMX8MM_RAM_SIZE_FIX)) {
@@ -73,7 +73,7 @@ static void spl_dram_init(void)
 		else if (IS_ENABLED(CONFIG_PHYCORE_IMX8MM_RAM_SIZE_4GB))
 			size = 5;
 	} else {
-		size = phytec_get_imx8m_ddr_size();
+		size = phytec_get_imx8m_ddr_size(NULL);
 	}
 
 	switch (size) {

commit 582d6fa353ccc9ba459c872c630f3e97ea49b892

board: phytec: common: fix som detection linker error
commit 7abca023ec45 ("boards: phytec: common: Split SOM detection")
introduced a bug that results in a linker error when deactivating SoM
detection via kconfig (for imx8m boards). Fix it by moving the function
definitions to the .c file. Also change the Makefile to always build
the *som_detection.o instead of conditional build.

Signed-off-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Teresa Remmet <t.remmet@phytec.de>

board/phytec/common/Makefile
@@ -7,5 +7,4 @@ ifdef CONFIG_SPL_BUILD
 obj- := __dummy__.o
 endif
 
-obj-$(CONFIG_PHYTEC_SOM_DETECTION) += phytec_som_detection.o
-obj-$(CONFIG_PHYTEC_IMX8M_SOM_DETECTION) += imx8m_som_detection.o
+obj-y += phytec_som_detection.o imx8m_som_detection.o

board/phytec/common/imx8m_som_detection.c
@@ -24,6 +24,9 @@ extern struct phytec_eeprom_data eeprom_data;
  *
  * Returns 0 in case it's a known SoM. Otherwise, returns -1.
  */
+
+#if CONFIG_IS_ENABLED(PHYTEC_IMX8M_SOM_DETECTION)
+
 u8 phytec_imx8m_detect(u8 som, char *opt)
 {
 	if (som == PHYTEC_IMX8MP_SOM && is_imx8mp())
@@ -150,3 +153,28 @@ u8 __maybe_unused phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data)
 	debug("%s: rtc: %u\n", __func__, rtc);
 	return rtc;
 }
+
+#else
+
+inline u8 __maybe_unused
+phytec_get_imx8m_ddr_size(struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+inline u8 __maybe_unused phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+inline u8 __maybe_unused phytec_get_imx8m_spi(struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+inline u8 __maybe_unused phytec_get_imx8m_eth(struct phytec_eeprom_data *data)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+#endif /* CONFIG_IS_ENABLED(PHYTEC_IMX8M_SOM_DETECTION) */

board/phytec/common/imx8m_som_detection.h
@@ -9,38 +9,10 @@
 
 #include "phytec_som_detection.h"
 
-#if IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION)
-
 u8 phytec_imx8m_detect(u8 som, char *opt);
 u8 __maybe_unused phytec_get_imx8m_ddr_size(struct phytec_eeprom_data *data);
 u8 __maybe_unused phytec_get_imx8mp_rtc(struct phytec_eeprom_data *data);
 u8 __maybe_unused phytec_get_imx8m_spi(struct phytec_eeprom_data *data);
 u8 __maybe_unused phytec_get_imx8m_eth(struct phytec_eeprom_data *data);
-
-#else
-
-inline u8 __maybe_unused phytec_get_imx8m_ddr_size(
-		struct phytec_eeprom_data *data)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline u8 __maybe_unused phytec_get_imx8mp_rtc(
-		struct phytec_eeprom_data *data)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline u8 __maybe_unused phytec_get_imx8m_spi(struct phytec_eeprom_data *data)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline u8 __maybe_unused phytec_get_imx8m_eth(struct phytec_eeprom_data *data)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-#endif /* IS_ENABLED(CONFIG_PHYTEC_IMX8M_SOM_DETECTION) */
 
 #endif /* _PHYTEC_IMX8M_SOM_DETECTION_H */

board/phytec/common/phytec_som_detection.c
@@ -17,6 +17,8 @@
 
 struct phytec_eeprom_data eeprom_data;
 
+#if CONFIG_IS_ENABLED(PHYTEC_SOM_DETECTION)
+
 int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
 			     int bus_num, int addr, int addr_fallback)
 {
@@ -207,3 +209,28 @@ char * __maybe_unused phytec_get_opt(struct phytec_eeprom_data *data)
 
 	return opt;
 }
+
+#else
+
+inline int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
+				    int bus_num, int addr, int addr_fallback)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+inline int phytec_eeprom_data_init(struct phytec_eeprom_data *data, int bus_num,
+				   int addr)
+{
+	return PHYTEC_EEPROM_INVAL;
+}
+
+inline void __maybe_unused phytec_print_som_info(struct phytec_eeprom_data *data)
+{
+}
+
+inline char *__maybe_unused phytec_get_opt(struct phytec_eeprom_data *data)
+{
+	return NULL;
+}
+
+#endif /* CONFIG_IS_ENABLED(PHYTEC_SOM_DETECTION) */

board/phytec/common/phytec_som_detection.h
@@ -57,8 +57,6 @@ struct phytec_eeprom_data {
 	} data;
 } __attribute__ ((__packed__));
 
-#if IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION)
-
 int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
 			     int bus_num, int addr, int addr_fallback);
 int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
@@ -66,31 +64,5 @@ int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
 void __maybe_unused phytec_print_som_info(struct phytec_eeprom_data *data);
 
 char * __maybe_unused phytec_get_opt(struct phytec_eeprom_data *data);
-
-# else
-
-inline int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
-				    int bus_num, int addr, int addr_fallback)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
-				   int bus_num, int addr)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline void __maybe_unused phytec_print_som_info(
-		struct phytec_eeprom_data *data)
-{
-}
-
-inline char * __maybe_unused phytec_get_opt(struct phytec_eeprom_data *data)
-{
-	return NULL;
-}
-
-#endif /* IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION) */
 
 #endif /* _PHYTEC_SOM_DETECTION_H */