a4de4459d1d8891158d3a4b66cdf49466907bd22
Benjamin Hahn <B.Hahn@phytec.de>
2023-08-04 08:44:53 +0200

    board: phytec: phycore_imx8mp: spl: add RAM autodetection
    If the EEPROM data is not available, because the EEPROM is overwritten
    for example, try to determine the correct timings for the RAM Modules.
    Reset the board on failure because the RAM can not be initialized after
    a failed attempt. The bootcounter will be stored at the beginning of the
    OCRAM_S. The OCRAM_S will persist the bootcounter over a warm reset, but
    not over a cold reset. The bootcounter will also be overwritten by the
    ddr_init function, which writes the RAM-Timings into the first 96 bytes
    of the OCRAM_S.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Reviewed-by: Yannic Moog <y.moog@phytec.de>
    Reviewed-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/phycore_imx8mp/spl.c
+++ board/phytec/phycore_imx8mp/spl.c
@@ -5,6 +5,7 @@
  */
 
 #include <common.h>
+#include <command.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/ddr.h>
 #include <asm/arch/imx8mp_pins.h>
@@ -25,22 +26,157 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#define EEPROM_ADDR		0x51
-#define EEPROM_ADDR_FALLBACK	0x59
+#define EEPROM_ADDR			0x51
+#define EEPROM_ADDR_FALLBACK		0x59
+#define MODE_REG_VAL_1GB_RAM		0xff030008
+#define MODE_REG_VAL_2GB_4GB_RAM	0xff000110
+#define INIT_1GB_RAM_BOOTCOUNTER	3
+
+static void warm_reset_pmic(void)
+{
+	struct pmic *p = pmic_get("PCA9450");
+
+	pmic_reg_write(p, PCA9450_SW_RST, 0x35);
+}
 
 int spl_board_boot_device(enum boot_device boot_dev_spl)
 {
 	return BOOT_DEVICE_BOOTROM;
 }
 
+void set_4gb_dram_timing(void)
+{
+	dram_timing.ddrc_cfg[2].val = 0xa3080020;
+	dram_timing.ddrc_cfg[39].val = 0x17;
+	dram_timing.fsp_msg[0].fsp_cfg[9].val = 0x310;
+	dram_timing.fsp_msg[0].fsp_cfg[21].val = 0x3;
+	dram_timing.fsp_msg[1].fsp_cfg[10].val = 0x310;
+	dram_timing.fsp_msg[1].fsp_cfg[22].val = 0x3;
+	dram_timing.fsp_msg[2].fsp_cfg[10].val = 0x310;
+	dram_timing.fsp_msg[2].fsp_cfg[22].val = 0x3;
+	dram_timing.fsp_msg[3].fsp_cfg[10].val = 0x310;
+	dram_timing.fsp_msg[3].fsp_cfg[22].val = 0x3;
+}
+
+void set_1gb_dram_timing(void)
+{
+	dram_timing.ddrc_cfg[3].val = 0x1233;
+	dram_timing.ddrc_cfg[5].val = 0x5b0087;
+	dram_timing.ddrc_cfg[6].val = 0x61027f10;
+	dram_timing.ddrc_cfg[7].val = 0x7b0;
+	dram_timing.ddrc_cfg[11].val = 0xf30000;
+	dram_timing.ddrc_cfg[23].val = 0x8d;
+	dram_timing.ddrc_cfg[44].val = 0xf070707;
+	dram_timing.ddrc_cfg[58].val = 0x1031;
+	dram_timing.ddrc_cfg[61].val = 0xc0012;
+	dram_timing.ddrc_cfg[76].val = 0x13;
+	dram_timing.ddrc_cfg[83].val = 0x1031;
+	dram_timing.ddrc_cfg[86].val = 0x30005;
+	dram_timing.ddrc_cfg[101].val = 0x5;
+	dram_timing.ddrphy_cfg[75].val = 0x1e3;
+	dram_timing.ddrphy_cfg[77].val = 0x1e3;
+	dram_timing.ddrphy_cfg[79].val = 0x1e3;
+	dram_timing.fsp_msg[0].fsp_cfg[11].val = 0xf3;
+	dram_timing.fsp_msg[0].fsp_cfg[16].val = 0xf3;
+	dram_timing.fsp_msg[0].fsp_cfg[23].val = 0xf32d;
+	dram_timing.fsp_msg[0].fsp_cfg[29].val = 0xf32d;
+	dram_timing.fsp_msg[3].fsp_cfg[12].val = 0xf3;
+	dram_timing.fsp_msg[3].fsp_cfg[17].val = 0xf3;
+	dram_timing.fsp_msg[3].fsp_cfg[24].val = 0xf32d;
+	dram_timing.fsp_msg[3].fsp_cfg[30].val = 0xf32d;
+}
+
+static unsigned int imx8mp_lpddr4_get_mr(void)
+{
+	unsigned int ddr_info = 0;
+	unsigned int const regs[] = { 5, 6, 7, 8 };
+
+	for (int attempts = 4; attempts >= 0; attempts--) {
+		for (int i = 0 ; i < ARRAY_SIZE(regs) ; i++) {
+			unsigned int data = lpddr4_mr_read(0xF, regs[i]);
+
+			data >>= 8;
+			ddr_info <<= 8;
+			ddr_info += (data & 0xFF);
+		}
+
+		if (ddr_info != 0xFFFFFFFF && ddr_info != 0)
+			break;
+
+		printf("read mr attempt failed, %i attempts left\n", attempts);
+	}
+	return  ddr_info;
+}
+
+static void spl_ram_autodetect(u8 bootcounter)
+{
+	int ret = 0;
+	int mode_reg = 0;
+	u8 ram_size = 0;
+	u8 *const bootcounter_ocram_s = (u8 *)CONFIG_SAVED_DRAM_TIMING_BASE;
+
+	switch (bootcounter) {
+	case 1:
+		set_4gb_dram_timing();
+		ram_size = 4;
+		break;
+	case 2:
+		ram_size = 2;
+		break;
+	case 3:
+		ram_size = 1;
+		set_1gb_dram_timing();
+		break;
+	default:
+		printf("failed to find any matching RAM timings\n");
+		do_reset(NULL, 0, 0, NULL);
+	}
+
+	ret = ddr_init(&dram_timing);
+	if (ret) {
+		if (bootcounter == 3) {
+			printf("RAM Autodetection failed\n");
+			do_reset(NULL, 0, 0, NULL);
+		} else {
+			*bootcounter_ocram_s = bootcounter + 1;
+			warm_reset_pmic();
+		}
+	}
+
+	mode_reg = imx8mp_lpddr4_get_mr();
+	if (mode_reg == MODE_REG_VAL_1GB_RAM && bootcounter != INIT_1GB_RAM_BOOTCOUNTER) {
+		*bootcounter_ocram_s = 3;
+		warm_reset_pmic();
+	} else if (mode_reg == MODE_REG_VAL_2GB_4GB_RAM &&
+		   bootcounter == INIT_1GB_RAM_BOOTCOUNTER) {
+		*bootcounter_ocram_s = 1;
+		warm_reset_pmic();
+	} else if (mode_reg != MODE_REG_VAL_2GB_4GB_RAM &&
+		   mode_reg != MODE_REG_VAL_1GB_RAM) {
+		printf("error: mode register values do not match a valid RAM config\n");
+	} else {
+		printf("detected %uGB RAM\n", ram_size);
+	}
+}
+
 void spl_dram_init(void)
 {
-	int ret;
+	int ret = 1;
 	int size = 0xf;
+	u8 *const bootcounter_ocram_s = (u8 *)CONFIG_SAVED_DRAM_TIMING_BASE;
+	u8 bootcounter = *bootcounter_ocram_s;
 
-	ret = phytec_eeprom_data_setup(NULL, 0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
-	if (ret && !IS_ENABLED(CONFIG_PHYCORE_IMX8MP_RAM_SIZE_FIX))
-		goto err;
+	if (bootcounter >= 4)
+		bootcounter = 1;
+	if (bootcounter == 1)
+		ret = phytec_eeprom_data_setup(NULL, 0, EEPROM_ADDR, EEPROM_ADDR_FALLBACK);
+
+	if (ret && !IS_ENABLED(CONFIG_PHYCORE_IMX8MP_RAM_SIZE_FIX)) {
+		printf("EEPROM detection failed and fixed RAM size not set, falling back to " \
+		       "autodetection\n");
+		spl_ram_autodetect(bootcounter);
+		return;
+	}
 
 	if (!ret) {
 		printf("phytec_eeprom_data_init: init successful\n");
@@ -57,63 +193,31 @@ void spl_dram_init(void)
 	} else {
 		size = phytec_get_imx8m_ddr_size(NULL);
 	}
-
 	switch (size) {
 	case 2:
-		/* 1GB */
-		dram_timing.ddrc_cfg[3].val = 0x1233;
-		dram_timing.ddrc_cfg[5].val = 0x5b0087;
-		dram_timing.ddrc_cfg[6].val = 0x61027f10;
-		dram_timing.ddrc_cfg[7].val = 0x7b0;
-		dram_timing.ddrc_cfg[11].val = 0xf30000;
-		dram_timing.ddrc_cfg[23].val = 0x8d;
-		dram_timing.ddrc_cfg[44].val = 0xf070707;
-		dram_timing.ddrc_cfg[58].val = 0x1031;
-		dram_timing.ddrc_cfg[61].val = 0xc0012;
-		dram_timing.ddrc_cfg[76].val = 0x13;
-		dram_timing.ddrc_cfg[83].val = 0x1031;
-		dram_timing.ddrc_cfg[86].val = 0x30005;
-		dram_timing.ddrc_cfg[101].val = 0x5;
-		dram_timing.ddrphy_cfg[75].val = 0x1e3;
-		dram_timing.ddrphy_cfg[77].val = 0x1e3;
-		dram_timing.ddrphy_cfg[79].val = 0x1e3;
-		dram_timing.fsp_msg[0].fsp_cfg[11].val = 0xf3;
-		dram_timing.fsp_msg[0].fsp_cfg[16].val = 0xf3;
-		dram_timing.fsp_msg[0].fsp_cfg[23].val = 0xf32d;
-		dram_timing.fsp_msg[0].fsp_cfg[29].val = 0xf32d;
-		dram_timing.fsp_msg[3].fsp_cfg[12].val = 0xf3;
-		dram_timing.fsp_msg[3].fsp_cfg[17].val = 0xf3;
-		dram_timing.fsp_msg[3].fsp_cfg[24].val = 0xf32d;
-		dram_timing.fsp_msg[3].fsp_cfg[30].val = 0xf32d;
-		ddr_init(&dram_timing);
+		set_1gb_dram_timing();
+		if (ddr_init(&dram_timing))
+			goto err;
+		printf("dram init complete for 1GB\n");
 		break;
 	case 3:
-		/* 2GB */
-		ddr_init(&dram_timing);
+		if (ddr_init(&dram_timing))
+			goto err;
+		printf("dram init complete for 2GB\n");
 		break;
 	case 5:
-		/* 4GB */
-		dram_timing.ddrc_cfg[2].val = 0xa3080020;
-		dram_timing.ddrc_cfg[39].val = 0x17;
-		dram_timing.fsp_msg[0].fsp_cfg[9].val = 0x310;
-		dram_timing.fsp_msg[0].fsp_cfg[21].val = 0x3;
-		dram_timing.fsp_msg[1].fsp_cfg[10].val = 0x310;
-		dram_timing.fsp_msg[1].fsp_cfg[22].val = 0x3;
-		dram_timing.fsp_msg[2].fsp_cfg[10].val = 0x310;
-		dram_timing.fsp_msg[2].fsp_cfg[22].val = 0x3;
-		dram_timing.fsp_msg[3].fsp_cfg[10].val = 0x310;
-		dram_timing.fsp_msg[3].fsp_cfg[22].val = 0x3;
-		ddr_init(&dram_timing);
+		set_4gb_dram_timing();
+		if (ddr_init(&dram_timing))
+			goto err;
+		printf("dram init complete for 4GB\n");
 		break;
 	default:
 		goto err;
 	}
-
 	return;
-
 err:
-	printf("Could not detect correct RAM size. Fallback to default.\n");
-	ddr_init(&dram_timing);
+	printf("Could not detect correct RAM size.\n");
+	do_reset(NULL, 0, 0, NULL);
 }
 
 #define I2C_PAD_CTRL (PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE | PAD_CTL_PE)
