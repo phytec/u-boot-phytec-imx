phytec/u-boot-phytec-imx

commit d4d8340ae7d893ef70ed8f385173dcf954ab3ee2

include: configs: Add RAUC boot logic
Add the RAUC boot logic for A/B systems into a separated header file.
This way we can avoid duplicating the implementation of that boot logic.
For now, the phyCORE-i.MX8M Mini, Nano and Plus make use of this, but
other modules could be easily added as explained in
include/configs/phycore_rauc_env.h.

squashed and adapted from v2021.04_2.2.0-phy:
07c28eaad1c1 include: configs: Add RAUC boot logic
b8bd81692e41 phycore_imx8mp.h: Add rauc support for i.MX8MP
8397d2fbff79 phycore_imx8mn.h: Add rauc support for i.MX8MN
2996bf0702f1 include: configs: phycore_imx8m{m, n, p}: Don't set doraucboot in default env
170bc9573f06 include: configs: phycore_imx8m*: Rework default setting of doraucboot
a89ba2080bf7 include: configs: phycore_rauc_env.h: Change partition numbering
51dccb5030b3 include: configs: phycore_imx8mm: Remove unneeded saveenv
ae7818d5d0c3 include: configs: phycore_imx8mn: Remove unneeded saveenv
f3333f36274d include: configs: phycore_imx8mp: Remove unneeded saveenv
cb83c96122f7 include: configs: phycore_rauc_env: Add Device Tree Overlay support

Signed-off-by: Martin Schwan <m.schwan@phytec.de>
Tested-by: Leonard Anderweit <l.anderweit@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

configs/phycore-imx8mm_defconfig
@@ -28,7 +28,6 @@ CONFIG_SPL_LOAD_FIT=y
 CONFIG_OF_BOARD_SETUP=y
 CONFIG_OF_SYSTEM_SETUP=y
 CONFIG_USE_BOOTCOMMAND=y
-CONFIG_BOOTCOMMAND="mmc dev ${mmcdev}; if mmc rescan; then if run loadimage; then run mmcboot; else run netboot; fi; fi;"
 CONFIG_DEFAULT_FDT_FILE="oftree"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_SPL_BOARD_INIT=y

configs/phycore-imx8mn_defconfig
@@ -31,7 +31,6 @@ CONFIG_FIT_EXTERNAL_OFFSET=0x3000
 CONFIG_SPL_LOAD_FIT=y
 CONFIG_OF_SYSTEM_SETUP=y
 CONFIG_USE_BOOTCOMMAND=y
-CONFIG_BOOTCOMMAND="mmc dev ${mmcdev}; if mmc rescan; then if run loadimage; then run mmcboot; else run netboot; fi; fi;"
 CONFIG_DEFAULT_FDT_FILE="oftree"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_SPL_BOARD_INIT=y

configs/phycore-imx8mp_defconfig
@@ -30,7 +30,6 @@ CONFIG_SPL_LOAD_FIT=y
 CONFIG_OF_BOARD_SETUP=y
 CONFIG_OF_SYSTEM_SETUP=y
 CONFIG_USE_BOOTCOMMAND=y
-CONFIG_BOOTCOMMAND="mmc dev ${mmcdev}; if mmc rescan; then if run loadimage; then run mmcboot; else run netboot; fi; fi;"
 CONFIG_DEFAULT_FDT_FILE="oftree"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_SPL_BOARD_INIT=y

include/configs/phycore_imx8mm.h
@@ -11,6 +11,8 @@
 #include <linux/stringify.h>
 #include <asm/arch/imx-regs.h>
 
+#include "phycore_rauc_env.h"
+
 #define CONFIG_SYS_BOOTM_LEN		SZ_64M
 #define CONFIG_SPL_MAX_SIZE		(148 * SZ_1K)
 #define CONFIG_SYS_MONITOR_LEN		SZ_512K
@@ -133,6 +135,19 @@
 		"else " \
 			"echo WARN: Cannot load the DT; " \
 		"fi;\0" \
+	"raucdev=2\0" \
+	PHYCORE_RAUC_ENV_BOOTLOGIC
+
+#define CONFIG_BOOTCOMMAND \
+	"mmc dev ${mmcdev}; if mmc rescan; then " \
+		"env exists doraucboot || setenv doraucboot 0;" \
+		"if test ${doraucboot} = 1; then " \
+			"run raucboot; " \
+		"elif run loadimage; then " \
+			"run mmcboot; " \
+		"else run netboot; " \
+		"fi; " \
+	"fi;"
 
 /* Link Definitions */
 

include/configs/phycore_imx8mn.h
@@ -11,6 +11,8 @@
 #include <linux/stringify.h>
 #include <asm/arch/imx-regs.h>
 
+#include "phycore_rauc_env.h"
+
 #define CONFIG_SYS_BOOTM_LEN		SZ_64M
 #define CONFIG_SPL_MAX_SIZE		(148 * SZ_1K)
 #define CONFIG_SYS_MONITOR_LEN		SZ_512K
@@ -126,6 +128,19 @@
 		"else " \
 			"echo WARN: Cannot load the DT; " \
 		"fi;\0" \
+	"raucdev=2\0" \
+	PHYCORE_RAUC_ENV_BOOTLOGIC
+
+#define CONFIG_BOOTCOMMAND \
+	"mmc dev ${mmcdev}; if mmc rescan; then " \
+		"env exists doraucboot || setenv doraucboot 0;" \
+		"if test ${doraucboot} = 1; then " \
+			"run raucboot; " \
+		"elif run loadimage; then " \
+			"run mmcboot; " \
+		"else run netboot; " \
+		"fi; " \
+	"fi;"
 
 /* Link Definitions */
 #define CONFIG_SYS_INIT_RAM_ADDR	0x40000000

include/configs/phycore_imx8mp.h
@@ -10,6 +10,8 @@
 #include <linux/sizes.h>
 #include <asm/arch/imx-regs.h>
 
+#include "phycore_rauc_env.h"
+
 #define CONFIG_SYS_BOOTM_LEN		SZ_64M
 
 #define CONFIG_SPL_MAX_SIZE		(152 * SZ_1K)
@@ -132,6 +134,19 @@
 		"else " \
 			"echo WARN: Cannot load the DT; " \
 		"fi;\0" \
+	"raucdev=2\0" \
+	PHYCORE_RAUC_ENV_BOOTLOGIC
+
+#define CONFIG_BOOTCOMMAND \
+	"mmc dev ${mmcdev}; if mmc rescan; then " \
+		"env exists doraucboot || setenv doraucboot 0;" \
+		"if test ${doraucboot} = 1; then " \
+			"run raucboot; " \
+		"elif run loadimage; then " \
+			"run mmcboot; " \
+		"else run netboot; " \
+		"fi; " \
+	"fi;"
 
 /* Link Definitions */
 

include/configs/phycore_rauc_env.h
@@ -1,0 +1,125 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2020 PHYTEC Messtechnik GmbH
+ * Author: Martin Schwan <m.schwan@phytec.de>
+ */
+
+#ifndef __PHYCORE_RAUC_ENV_H
+#define __PHYCORE_RAUC_ENV_H
+
+/*
+ * Note:
+ *
+ * These are environment variables and scripts that help booting an A/B system
+ * using RAUC (see https://rauc.io) on PHYTEC's phyCORE SoMs.
+ *
+ * Implementation:
+ *
+ *  1. Include this file in your board/SoM configuration.
+ *  2. Set the following variables in the U-Boot environment:
+ *     a. "doraucboot" for the default value whether to boot the A/B system.
+ *     b. "raucdev" the MMC device index that the A/B system is installed on. To
+ *        get a list of available MMC devices in U-Boot use "mmc list".
+ *  3. Include the macro PHYCORE_RAUC_ENV_BOOTLOGIC environment to add the boot
+ *     logic for A/B systems.
+ *  4. Make sure CONFIG_BOOTCOMMAND properly boots the A/B system by executing
+ *     "run raucboot" depending on the value of "doraucboot".
+ *
+ * Usage:
+ *
+ *  1. Set "doraucboot" to "1" to activate booting the A/B system and save this
+ *     setting with "saveenv".
+ *  2. Run "boot".
+ */
+
+#define PHYCORE_RAUC_ENV_BOOTLOGIC \
+	"raucslot=system0\0" \
+	"raucbootpart=1\0" \
+	"raucrootpart=5\0" \
+	"raucbootpart0=1\0" \
+	"raucrootpart0=5\0" \
+	"raucbootpart1=2\0" \
+	"raucrootpart1=6\0" \
+	"raucargs=setenv bootargs console=${console} " \
+		"root=/dev/mmcblk${raucdev}p${raucrootpart} " \
+		"rauc.slot=${raucslot} rootwait rw\0" \
+	"loadraucimage=fatload mmc ${raucdev}:${raucbootpart} ${loadaddr} ${image}\0" \
+	"loadraucfdt=fatload mmc ${raucdev}:${raucbootpart} ${fdt_addr} ${fdt_file}\0" \
+	"loadrauc_bootenv=fatload mmc ${raucdev}:${raucbootpart} ${bootenv_addr} ${bootenv}\0" \
+	"rauc_apply_overlays=fdt address ${fdt_addr}; "	\
+		"if test ${no_extensions} = 0; then " \
+			"setenv extension_overlay_addr ${fdto_addr}; " \
+			"setenv extension_overlay_cmd \'fatload mmc ${raucdev}:${raucbootpart} " \
+				"${fdto_addr} ${extension_overlay_name}\'; " \
+			"extension scan; " \
+			"extension apply all; " \
+		"fi; " \
+		"if test ${no_overlays} = 0; then " \
+			"for overlay in $overlays; do " \
+				"if run mmc_load_overlay; then " \
+					"fdt resize ${filesize}; " \
+					"fdt apply ${fdto_addr}; " \
+				"fi; " \
+			"done;" \
+		"fi;\0" \
+	"raucemmcboot=echo Booting A/B system from eMMC...; " \
+		"if run loadrauc_bootenv; then " \
+			"env import -t ${bootenv_addr} ${filesize}; " \
+		"fi; " \
+		"if run loadraucimage; then " \
+			"run raucargs; " \
+			"if run loadraucfdt; then " \
+				"run rauc_apply_overlays; " \
+				"booti ${loadaddr} - ${fdt_addr}; " \
+			"else " \
+				"echo WARN: Cannot load device tree; " \
+			"fi; " \
+		"else " \
+			"echo WARN: Cannot load kernel image; " \
+		"fi;\0" \
+	"raucboot=" \
+		"test -n \"${BOOT_ORDER}\" || setenv BOOT_ORDER \"system0 system1\"; " \
+		"test -n \"${BOOT_system0_LEFT}\" || setenv BOOT_system0_LEFT 3; " \
+		"test -n \"${BOOT_system1_LEFT}\" || setenv BOOT_system1_LEFT 3; " \
+		"setenv raucstatus; " \
+		"for BOOT_SLOT in \"${BOOT_ORDER}\"; do " \
+			"if test \"x${raucstatus}\" != \"x\"; then " \
+				"echo Skipping remaing slots!; " \
+			"elif test \"x${BOOT_SLOT}\" = \"xsystem0\"; then " \
+				"if test ${BOOT_system0_LEFT} -gt 0; then " \
+					"echo \"Found valid slot A, " \
+						"${BOOT_system0_LEFT} attempts remaining\"; " \
+					"setexpr BOOT_system0_LEFT ${BOOT_system0_LEFT} - 1; " \
+					"setenv raucbootpart ${raucbootpart0}; " \
+					"setenv raucrootpart ${raucrootpart0}; " \
+					"setenv raucslot system0; " \
+					"run raucargs; " \
+					"setenv raucstatus success; " \
+				"fi; " \
+			"elif test \"x${BOOT_SLOT}\" = \"xsystem1\"; then " \
+				"if test ${BOOT_system1_LEFT} -gt 0; then " \
+					"echo \"Found valid slot B, " \
+						"${BOOT_system1_LEFT} attempts remaining\"; " \
+					"setexpr BOOT_system1_LEFT ${BOOT_system1_LEFT} - 1; " \
+					"setenv raucbootpart ${raucbootpart1}; " \
+					"setenv raucrootpart ${raucrootpart1}; " \
+					"setenv raucslot system1; " \
+					"run raucargs; " \
+					"setenv raucstatus success; " \
+				"fi; " \
+			"fi; " \
+		"done; " \
+		"if test -n \"${raucstatus}\"; then " \
+			"env delete raucstatus; " \
+			"saveenv; " \
+			"run raucemmcboot; " \
+		"else " \
+			"echo \"WARN: No valid slot found\"; " \
+			"setenv BOOT_system0_LEFT 3; " \
+			"setenv BOOT_system1_LEFT 3; " \
+			"env delete raucstatus; " \
+			"saveenv; " \
+			"reset; " \
+		"fi;\0"
+
+#endif /* __PHYCORE_RAUC_ENV_H */

commit dff640e25d61121b9c76b4a3f077a961965e3f11

configs: phycore-imx8{m, n, p}: Move CONFIG_BOOTCOMMAND to defconfig
Move CONFIG_BOOTCOMMAND to defconfig. This fixes compiler warnings:

  warning: "CONFIG_BOOTCOMMAND" redefined

Fixes: d4d8340ae7d8 ("include: configs: Add RAUC boot logic")
Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
Reviewed-by: Martin Schwan <m.schwan@phytec.de>

configs/phycore-imx8mm_defconfig
@@ -28,6 +28,7 @@ CONFIG_SPL_LOAD_FIT=y
 CONFIG_OF_BOARD_SETUP=y
 CONFIG_OF_SYSTEM_SETUP=y
 CONFIG_USE_BOOTCOMMAND=y
+CONFIG_BOOTCOMMAND="mmc dev ${mmcdev}; if mmc rescan; then env exists doraucboot || setenv doraucboot 0; if test ${doraucboot} = 1; then run raucboot; elif run loadimage; then run mmcboot; else run netboot; fi; fi;"
 CONFIG_DEFAULT_FDT_FILE="oftree"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_SPL_BOARD_INIT=y

configs/phycore-imx8mn_defconfig
@@ -31,6 +31,7 @@ CONFIG_FIT_EXTERNAL_OFFSET=0x3000
 CONFIG_SPL_LOAD_FIT=y
 CONFIG_OF_SYSTEM_SETUP=y
 CONFIG_USE_BOOTCOMMAND=y
+CONFIG_BOOTCOMMAND="mmc dev ${mmcdev}; if mmc rescan; then env exists doraucboot || setenv doraucboot 0; if test ${doraucboot} = 1; then run raucboot; elif run loadimage; then run mmcboot; else run netboot; fi; fi;"
 CONFIG_DEFAULT_FDT_FILE="oftree"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_SPL_BOARD_INIT=y

configs/phycore-imx8mp_defconfig
@@ -30,6 +30,7 @@ CONFIG_SPL_LOAD_FIT=y
 CONFIG_OF_BOARD_SETUP=y
 CONFIG_OF_SYSTEM_SETUP=y
 CONFIG_USE_BOOTCOMMAND=y
+CONFIG_BOOTCOMMAND="mmc dev ${mmcdev}; if mmc rescan; then env exists doraucboot || setenv doraucboot 0; if test ${doraucboot} = 1; then run raucboot; elif run loadimage; then run mmcboot; else run netboot; fi; fi;"
 CONFIG_DEFAULT_FDT_FILE="oftree"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_SPL_BOARD_INIT=y

include/configs/phycore_imx8mm.h
@@ -138,17 +138,6 @@
 	"raucdev=2\0" \
 	PHYCORE_RAUC_ENV_BOOTLOGIC
 
-#define CONFIG_BOOTCOMMAND \
-	"mmc dev ${mmcdev}; if mmc rescan; then " \
-		"env exists doraucboot || setenv doraucboot 0;" \
-		"if test ${doraucboot} = 1; then " \
-			"run raucboot; " \
-		"elif run loadimage; then " \
-			"run mmcboot; " \
-		"else run netboot; " \
-		"fi; " \
-	"fi;"
-
 #ifdef CONFIG_ENV_WRITEABLE_LIST
 /* Set environment flag validation to RAUC's list of env vars that must writable */
 #define CONFIG_ENV_FLAGS_LIST_STATIC RAUC_REQUIRED_WRITABLE_ENV_FLAGS

include/configs/phycore_imx8mn.h
@@ -131,17 +131,6 @@
 	"raucdev=2\0" \
 	PHYCORE_RAUC_ENV_BOOTLOGIC
 
-#define CONFIG_BOOTCOMMAND \
-	"mmc dev ${mmcdev}; if mmc rescan; then " \
-		"env exists doraucboot || setenv doraucboot 0;" \
-		"if test ${doraucboot} = 1; then " \
-			"run raucboot; " \
-		"elif run loadimage; then " \
-			"run mmcboot; " \
-		"else run netboot; " \
-		"fi; " \
-	"fi;"
-
 #ifdef CONFIG_ENV_WRITEABLE_LIST
 /* Set environment flag validation to RAUC's list of env vars that must writable */
 #define CONFIG_ENV_FLAGS_LIST_STATIC RAUC_REQUIRED_WRITABLE_ENV_FLAGS

include/configs/phycore_imx8mp.h
@@ -137,17 +137,6 @@
 	"raucdev=2\0" \
 	PHYCORE_RAUC_ENV_BOOTLOGIC
 
-#define CONFIG_BOOTCOMMAND \
-	"mmc dev ${mmcdev}; if mmc rescan; then " \
-		"env exists doraucboot || setenv doraucboot 0;" \
-		"if test ${doraucboot} = 1; then " \
-			"run raucboot; " \
-		"elif run loadimage; then " \
-			"run mmcboot; " \
-		"else run netboot; " \
-		"fi; " \
-	"fi;"
-
 #ifdef CONFIG_ENV_WRITEABLE_LIST
 /* Set environment flag validation to RAUC's list of env vars that must writable */
 #define CONFIG_ENV_FLAGS_LIST_STATIC RAUC_REQUIRED_WRITABLE_ENV_FLAGS