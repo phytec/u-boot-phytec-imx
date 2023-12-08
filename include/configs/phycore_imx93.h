5adc97f08ec603dd30a1a3027d8a4dd41831953a
Christoph Stoidner <c.stoidner@phytec.de>
2023-12-08 10:48:39 +0100

    board: phytec: imx93: Add PHYTEC phyCORE-i.MX93 support
    Add initial support for PHYTEC phyCORE-i.MX93 SOM.
    
    Supported features:
     - 1GB LPDDR4 RAM
     - external SD
     - debug UART
     - 1x 100Mbit Ethernet
     - USB
    
    Signed-off-by: Christoph Stoidner <c.stoidner@phytec.de>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    (cherry-pick and adapt from commit a562d1056087d405828881b9be14a30dabc0611f)
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

---
+++ include/configs/phycore_imx93.h
@@ -0,0 +1,76 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+ *
+ * Copyright (C) 2023 PHYTEC Messtechnik GmbH
+ * Author: Christoph Stoidner <c.stoidner@phytec.de>
+ */
+
+#ifndef __PHYCORE_IMX93_H
+#define __PHYCORE_IMX93_H
+
+#include <linux/sizes.h>
+#include <linux/stringify.h>
+#include <asm/arch/imx-regs.h>
+
+#define CFG_SYS_UBOOT_BASE	\
+	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
+
+/* Initial environment variables */
+#define CFG_EXTRA_ENV_SETTINGS		\
+	"image=Image\0" \
+	"console=ttyLP0\0" \
+	"fdt_addr=0x83000000\0" \
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"ipaddr=192.168.3.11\0" \
+	"serverip=192.168.3.10\0" \
+	"netmask=255.255.255.0\0" \
+	"ip_dyn=no\0" \
+	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
+	"mmcpart=1\0" \
+	"mmcroot=2\0" \
+	"mmcautodetect=yes\0" \
+	"mmcargs=setenv bootargs console=${console},${baudrate} earlycon " \
+		"root=/dev/mmcblk${mmcdev}p${mmcroot} rootwait rw\0" \
+	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
+	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"mmcboot=echo Booting from mmc ...; " \
+		"run mmcargs; " \
+		"if run loadfdt; then " \
+			"booti ${loadaddr} - ${fdt_addr}; " \
+		"else " \
+			"echo WARN: Cannot load the DT; " \
+		"fi;\0 " \
+	"nfsroot=/nfs\0" \
+	"netargs=setenv bootargs console=${console},${baudrate} earlycon " \
+		"root=/dev/nfs ip=${nfsip} " \
+		"nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
+	"netboot=echo Booting from net ...; " \
+		"if test ${ip_dyn} = yes; then " \
+			"setenv nfsip dhcp; " \
+			"setenv get_cmd dhcp; " \
+		"else " \
+			"setenv nfsip ${ipaddr}:${serverip}::${netmask}::eth0:on; " \
+			"setenv get_cmd tftp; " \
+		"fi; " \
+		"run netargs; " \
+		"${get_cmd} ${loadaddr} ${image}; " \
+		"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \
+			"booti ${loadaddr} - ${fdt_addr}; " \
+		"else " \
+			"echo WARN: Cannot load the DT; " \
+		"fi;\0" \
+
+/* Link Definitions */
+
+#define CFG_SYS_INIT_RAM_ADDR	0x80000000
+#define CFG_SYS_INIT_RAM_SIZE	0x200000
+
+#define CFG_SYS_SDRAM_BASE		0x80000000
+#define PHYS_SDRAM				0x80000000
+#define PHYS_SDRAM_SIZE			0x80000000
+
+#define CFG_SYS_FSL_USDHC_NUM	2
+
+/* Using ULP WDOG for reset */
+#define WDOG_BASE_ADDR			WDG3_BASE_ADDR
+
+#endif /* __PHYCORE_IMX93_H */
