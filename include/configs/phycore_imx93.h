14f802d1647bac117d110030467eb0a0a19a1df2
Primoz Fiser <primoz.fiser@norik.com>
2023-12-11 14:20:21 +0100

    board: phycore-imx93: Move environment to .env file
    Move environment from board C header files to .env file. This makes
    maintenance easier and environment more readable.
    
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- include/configs/phycore_imx93.h
+++ include/configs/phycore_imx93.h
@@ -8,87 +8,10 @@
 #define __PHYCORE_IMX93_H
 
 #include <linux/sizes.h>
-#include <linux/stringify.h>
 #include <asm/arch/imx-regs.h>
 
 #define CFG_SYS_UBOOT_BASE	\
 	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
-
-/* Initial environment variables */
-#define CFG_EXTRA_ENV_SETTINGS		\
-	"image=Image\0" \
-	"console=ttyLP0\0" \
-	"fdt_addr=0x83000000\0" \
-	"fdto_addr=0x830c0000\0" \
-	"bootenv_addr=0x83500000\0" \
-	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	"ipaddr=192.168.3.11\0" \
-	"serverip=192.168.3.10\0" \
-	"netmask=255.255.255.0\0" \
-	"ip_dyn=no\0" \
-	"bootenv=bootenv.txt\0" \
-	"mmc_load_bootenv=fatload mmc ${mmcdev}:${mmcpart} ${bootenv_addr} ${bootenv}\0" \
-	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=1\0" \
-	"mmcroot=2\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs console=${console},${baudrate} earlycon " \
-		"root=/dev/mmcblk${mmcdev}p${mmcroot} rootwait rw\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
-	"mmc_load_overlay=fatload mmc ${mmcdev}:${mmcpart} ${fdto_addr} ${overlay}\0" \
-	"mmc_apply_overlays=fdt address ${fdt_addr}; "  \
-		"for overlay in ${overlays}; " \
-		"do; " \
-			"if run mmc_load_overlay; then " \
-				"fdt resize ${filesize}; " \
-				"fdt apply ${fdto_addr}; " \
-			"fi; " \
-		"done;\0" \
-	"mmcboot=echo Booting from mmc ...; " \
-		"if run mmc_load_bootenv; then " \
-			"env import -t ${bootenv_addr} ${filesize}; " \
-		"fi; " \
-		"run mmcargs; " \
-		"if run loadfdt; then " \
-			"run mmc_apply_overlays; " \
-			"booti ${loadaddr} - ${fdt_addr}; " \
-		"else " \
-			"echo WARN: Cannot load the DT; " \
-		"fi;\0 " \
-	"nfsroot=/nfs\0" \
-	"netargs=setenv bootargs console=${console},${baudrate} earlycon " \
-		"root=/dev/nfs ip=${nfsip} " \
-		"nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"net_load_bootenv=${get_cmd} ${bootenv_addr} ${bootenv}\0" \
-	"net_load_overlay=${get_cmd} ${fdto_addr} ${overlay}\0" \
-	"net_apply_overlays=fdt address ${fdt_addr}; " \
-		"for overlay in ${overlays}; " \
-		"do; " \
-			"if run net_load_overlay; then " \
-				"fdt resize ${filesize}; " \
-				"fdt apply ${fdto_addr}; " \
-			"fi; " \
-		"done;\0" \
-	"netboot=echo Booting from net ...; " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv nfsip dhcp; " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv nfsip ${ipaddr}:${serverip}::${netmask}::eth0:on; " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"if run net_load_bootenv; then " \
-			"env import -t ${bootenv_addr} ${filesize}; " \
-		"fi; " \
-		"run netargs; " \
-		"${get_cmd} ${loadaddr} ${image}; " \
-		"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \
-			"run net_apply_overlays; " \
-			"booti ${loadaddr} - ${fdt_addr}; " \
-		"else " \
-			"echo WARN: Cannot load the DT; " \
-		"fi;\0" \
 
 /* Link Definitions */
 
