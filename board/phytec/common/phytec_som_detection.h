a107f34e926739fd84290754cb11dbcfb7ec986c
Yannic Moog <y.moog@phytec.de>
2024-02-19 09:25:39 +0100

    UPSTREAM: board: phytec: som_detection: move definitions to source file
    Move all function definitions in {phytec|imx8m}_som_detection from the
    header to the source file to prevent potential linker error regarding
    multiple definitions. Also move the #if blocks with the definitions.
    
    Signed-off-by: Yannic Moog <y.moog@phytec.de>
    (cherry-pick and adapt from commit aa7858fe5e2ef3d74129175c4044c9bb59174b4a)
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/common/phytec_som_detection.h
+++ board/phytec/common/phytec_som_detection.h
@@ -58,8 +58,6 @@ struct phytec_eeprom_data {
 	} data;
 } __packed;
 
-#if IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION)
-
 int phytec_eeprom_data_setup_fallback(struct phytec_eeprom_data *data,
 				      int bus_num, int addr,
 				      int addr_fallback);
@@ -74,49 +72,5 @@ u8 __maybe_unused phytec_get_rev(struct phytec_eeprom_data *data);
 
 struct extension *phytec_add_extension(const char *name, const char *overlay,
 				       const char *other);
-
-#else
-
-inline int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
-				    int bus_num, int addr)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline int phytec_eeprom_data_setup_fallback(struct phytec_eeprom_data *data,
-					     int bus_num, int addr,
-					     int addr_fallback)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
-				   int bus_num, int addr)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline void __maybe_unused phytec_print_som_info(struct phytec_eeprom_data *data)
-{
-}
-
-inline char *__maybe_unused phytec_get_opt(struct phytec_eeprom_data *data)
-{
-	return NULL;
-}
-
-u8 __maybe_unused phytec_get_rev(struct phytec_eeprom_data *data)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline struct extension *phytec_add_extension(const char *name,
-					      const char *overlay,
-					      const char *other)
-{
-	return NULL;
-}
-
-#endif /* IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION) */
 
 #endif /* _PHYTEC_SOM_DETECTION_H */
