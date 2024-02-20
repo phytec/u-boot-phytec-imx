77e441499091cf8126dd6e455a573cd81560adf2
Benjamin Hahn <B.Hahn@phytec.de>
2024-02-20 12:16:33 +0100

    board: phytec: common: phytec_som_detection: Add phytec_get_som_type
    Add a function that gets the som_type from the EEPROM.
    Add an enum for the som_type.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Reviewed-by: Yannic Moog <y.moog@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/common/phytec_som_detection.c
+++ board/phytec/common/phytec_som_detection.c
@@ -212,6 +212,16 @@ u8 __maybe_unused phytec_get_rev(struct phytec_eeprom_data *data)
 	return api2->pcb_rev;
 }
 
+u8 __maybe_unused phytec_get_som_type(struct phytec_eeprom_data *data)
+{
+	if (!data)
+		data = &eeprom_data;
+	if (!data->valid || data->data.api_rev < PHYTEC_API_REV2)
+		return PHYTEC_EEPROM_INVAL;
+
+	return data->data.data.data_api2.som_type;
+}
+
 struct extension *phytec_add_extension(const char *name, const char *overlay,
 				       const char *other)
 {
