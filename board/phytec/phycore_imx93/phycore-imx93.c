44df3154c240ab3ec8b58d2ec0fe7fe3438f4d78
Primoz Fiser <primoz.fiser@norik.com>
2024-08-23 13:39:57 +0200

    board: phytec: phycore-imx93: Implement phy fixups
    Add fixups for the TI DP83825/6I Ethernet PHYs. This is required since
    phyCORE-i.MX93 SoMs came initially equipped with DP83826I and later
    switched to DP83825I PHYs. The new PHYs should be a direct replacement
    but some strap resistors configuration differs. Moreover, LED setup was
    swapped already on the initial SOM revisions. Thus to avoid the need to
    replace HW components, handle PHY configuration with this fixup, making
    sure AutoNeg, CRS_DV and LEDs are enabled and configured appropriately.
    
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/phycore_imx93/phycore-imx93.c
+++ board/phytec/phycore_imx93/phycore-imx93.c
@@ -13,6 +13,7 @@
 #include <asm/mach-imx/boot_mode.h>
 #include <env.h>
 #include <extension_board.h>
+#include <phy.h>
 
 #include "../common/imx93_som_detection.h"
 
@@ -47,6 +48,59 @@ int board_late_init(void)
 	default:
 		break;
 	}
+
+	return 0;
+}
+
+#define DP83822_DEVADDR        0x1f
+
+static int dp8382x_phy_fixup(struct phy_device *phydev)
+{
+	int pid;
+
+	/* Read PHYIDR2 for model number */
+	pid = phy_read_mmd(phydev, DP83822_DEVADDR, 0x3);
+
+	/* DP83825I fixup */
+	if (((pid >> 4) & 0x3F) == 0x14) {
+		/* AutoNeg enable */
+		phy_set_bits_mmd(phydev, DP83822_DEVADDR, 0x0, BIT(12));
+		/* CRS_DV: enable */
+		phy_clear_bits_mmd(phydev, DP83822_DEVADDR, 0x302, BIT(8));
+		/* LED_2_Polarity: active low */
+		phy_clear_bits_mmd(phydev, DP83822_DEVADDR, 0x469, BIT(6));
+		/* LED_2_Control: RX/TX act */
+		phy_clear_bits_mmd(phydev, DP83822_DEVADDR, 0x460, GENMASK(7, 4));
+		phy_set_bits_mmd(phydev, DP83822_DEVADDR, 0x460, BIT(4));
+		/* LED_0_Configuration: Link OK, cfg_mled_en: LED_0 */
+		phy_clear_bits_mmd(phydev, DP83822_DEVADDR, 0x25, GENMASK(6, 3));
+		phy_set_bits_mmd(phydev, DP83822_DEVADDR, 0x25, BIT(0));
+		/* LED_Link_Polarity : active low */
+		phy_clear_bits_mmd(phydev, DP83822_DEVADDR, 0x18, BIT(7));
+	}
+
+	/* DP83826I fixup */
+	if (((pid >> 4) & 0x3F) == 0x11) {
+		/* Swap LEDs configuration */
+		phy_clear_bits_mmd(phydev, DP83822_DEVADDR, 0x460, GENMASK(3, 0));
+		phy_set_bits_mmd(phydev, DP83822_DEVADDR, 0x460, BIT(0));
+		phy_clear_bits_mmd(phydev, DP83822_DEVADDR, 0x25, GENMASK(6, 3));
+	}
+
+	return 0;
+}
+
+int board_phy_config(struct phy_device *phydev)
+{
+	u8 option = phytec_imx93_get_opt(NULL, PHYTEC_IMX93_OPT_ETH);
+
+	if (!option)
+		return 0;
+
+	dp8382x_phy_fixup(phydev);
+
+	if (phydev->drv->config)
+		return phydev->drv->config(phydev);
 
 	return 0;
 }
