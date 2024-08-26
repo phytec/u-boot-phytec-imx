varigit/uboot-imx

commit 4e175f663cddb4dc8abacb5dfc870f5cbccb7179

LFU-648-17 net: fec_mxc: Fix clk_ref rate on iMX93
Becuase iMX93 has a internal 1/2 divider before clock input to
network controller, so have to set twice frequency rate

Signed-off-by: Ye Li <ye.li@nxp.com>

drivers/net/fec_mxc.c
@@ -1218,6 +1218,9 @@ static int fecmxc_set_ref_clk(struct clk *clk_ref, phy_interface_t interface)
 	else
 		return -EINVAL;
 
+	if (is_imx93())
+		freq = freq << 1;
+
 	ret = clk_set_rate(clk_ref, freq);
 	if (ret < 0)
 		return ret;

phytec/u-boot-phytec-imx

commit 4f41a4bcb9b93b9779d4ce02cbeb3dc15e5f4c7a

net: fec_mxc: Remove clk_ref rate multiplication
NXP commit 4e175f663cdd ("LFU-648-17 net: fec_mxc: Fix clk_ref rate on
iMX93") introduced clk_ref rate multiplication for i.MX93 which breaks
FEC Ethernet on phyCORE-i.MX93 SoMs. However, in mainline U-Boot, FEC
Ethernet works just fine. Thus, fix this by removing NXP change.

Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

drivers/net/fec_mxc.c
@@ -1218,9 +1218,6 @@ static int fecmxc_set_ref_clk(struct clk *clk_ref, phy_interface_t interface)
 	else
 		return -EINVAL;
 
-	if (is_imx93() || is_imx91())
-		freq = freq << 1;
-
 	ret = clk_set_rate(clk_ref, freq);
 	if (ret < 0)
 		return ret;