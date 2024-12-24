55fad7d085b7b6101dd41cd3e0f7276c1a5f8923
Teresa Remmet <t.remmet@phytec.de>
2024-12-24 10:18:38 +0100

    configs: phycore-imx8mm_defconfig: Enable USB Host support
    Add USB Host support to load images from USB Stick on phyBOARD-Polis.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    (cherry-pick and adapt from commit cfad9ab5237bea17081b8322fb2705b460db39a9)
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Yunus Bas <y.bas@phytec.de>
    (cherry-pick and adapt from commit bbfa689e9dac31336c2d9ed3ceb7b25839ba9c72)
    Signed-off-by: Andrej Picej <andrej.picej@norik.com>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- include/configs/phycore_imx8mm.h
+++ include/configs/phycore_imx8mm.h
@@ -31,4 +31,9 @@
 #define PHYS_SDRAM			0x40000000
 #define PHYS_SDRAM_SIZE                 SZ_2G /* 2GB DDR */
 
+/* USB configs */
+#define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_USB_MAX_CONTROLLER_COUNT	2
+#define CONFIG_SERIAL_TAG
+
 #endif /* __PHYCORE_IMX8MM_H */
