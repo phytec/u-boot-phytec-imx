phytec/u-boot-phytec-imx

commit 626728c947ff2b38ce6557a349486bca686bc342

board: phytec: phycore-imx8mm: Implement env_get_location
To unify the SPI and MMC config decide during runtime which
environment we are using.

Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
Reviewed-by: Janine Hagemann <j.hagemann@phytec.de>
(cherry-pick and adapt from commit 31119e9bd042d36f3388a54dafa76baf69588078)
Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
(cherry-pick and adapt from commit b2fb0ef39ca75ded6f9ae882fd03c3c88ce1ddfd)
Signed-off-by: Andrej Picej <andrej.picej@norik.com>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/phycore_imx8mm/phycore-imx8mm.c
@@ -10,6 +10,7 @@
 #include <asm/io.h>
 #include <asm/mach-imx/boot_mode.h>
 #include <env.h>
+#include <env_internal.h>
 #include <miiphy.h>
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -51,4 +52,39 @@ int board_late_init(void)
 	}
 
 	return 0;
+}
+
+enum env_location env_get_location(enum env_operation op, int prio)
+{
+	enum boot_device dev = get_boot_device();
+	enum env_location env_loc = ENVL_UNKNOWN;
+
+	if (prio > 1)
+		return env_loc;
+
+	if (prio == 1) {
+		if (IS_ENABLED(CONFIG_ENV_APPEND))
+			return ENVL_NOWHERE;
+		else
+			return env_loc;
+	}
+
+	switch (dev) {
+	case QSPI_BOOT:
+		env_loc = ENVL_SPI_FLASH;
+		break;
+	case SD1_BOOT:
+	case SD2_BOOT:
+	case SD3_BOOT:
+	case MMC2_BOOT:
+	case MMC3_BOOT:
+	case USB_BOOT:
+		env_loc =  ENVL_MMC;
+		break;
+	default:
+		env_loc = ENVL_NOWHERE;
+		break;
+	}
+
+	return env_loc;
 }

commit 74f69ef443630394e006410200fa44284f290dda

board: phytec: phycore_imx8mm: phycore-imx8mm: Fix saveenv
Remove the fallback of reading the environment from nowhere as this is
already implemented in 5ab81058364b (env: Complete generic support for writable list).
This fixes saveenv when CONFIG_ENV_APPEND is enabled.

Fixes: 626728c947ff (board: phytec: phycore-imx8mm: Implement
env_get_location)

Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/phycore_imx8mm/phycore-imx8mm.c
@@ -159,15 +159,8 @@ enum env_location env_get_location(enum env_operation op, int prio)
 	enum boot_device dev = get_boot_device();
 	enum env_location env_loc = ENVL_UNKNOWN;
 
-	if (prio > 1)
+	if (prio > 0)
 		return env_loc;
-
-	if (prio == 1) {
-		if (IS_ENABLED(CONFIG_ENV_APPEND))
-			return ENVL_NOWHERE;
-		else
-			return env_loc;
-	}
 
 	switch (dev) {
 	case QSPI_BOOT: