c441036ad3c80522f2e1b5ed9d0972b1248dc90d
Teresa Remmet <t.remmet@phytec.de>
2025-03-18 09:12:15 +0100

    WORKAROUND: board: phytec: imx95-libra-fpsc: Make USB Hub functional
    The upstream ports of the USB Hub on Libra PCM-937-L 1618.0, 1618.1
    have swapped pin polarity.
    To workaround this we can set polarity in the USB Hub registers which
    are available over i2c.
    
    As on boards the i2c address may vary for yet unknown reasons we
    need to test all possible i2c addresses to write to the registers.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/imx95-libra-fpsc/imx95-libra-fpsc.c
+++ board/phytec/imx95-libra-fpsc/imx95-libra-fpsc.c
@@ -13,6 +13,7 @@
 #include <linux/bitfield.h>
 #include <linux/bitops.h>
 #include <linux/delay.h>
+#include <i2c.h>
 #include <miiphy.h>
 #include <asm/gpio.h>
 #include <asm/arch/sys_proto.h>
@@ -33,8 +34,44 @@ int board_early_init_f(void)
 	return 0;
 }
 
+#define TUSB_PORT_POL_CRTL_REG	0xB
+#define TUSB_CUSTOM_POL		BIT(7)
+#define TUSB_P0_POL		BIT(0)
+
+/*
+ * WORKAROUND for PCM-937-L 1618.0, 1618.1.
+ * USB HUB TUSB8042A has swapped upstream pin polarity.
+ * Set i2c registers to inform the hub that the lines
+ * are swapped.
+ *
+ * We also notice that the HUB i2c address might not be
+ * as expected for unknown reasons. Test all 4 possible i2c
+ * addresses to write to the device.
+ *
+ */
+void tusb8042a_swap_lines(void)
+{
+	const u8 pol_swap_val = (TUSB_CUSTOM_POL | TUSB_P0_POL);
+	const int addr[4] = {0x44, 0x45, 0x46, 0x47};
+	struct udevice *dev;
+	int i, ret;
+
+	for (i = 0; i < 4; i++) {
+		ret = i2c_get_chip_for_busnum(2, addr[i], 1, &dev);
+		if (!ret) {
+			dm_i2c_write(dev, TUSB_PORT_POL_CRTL_REG, &pol_swap_val, 1);
+			break;
+		};
+	}
+
+	if (ret)
+		printf("TUSB8042A: Failed to fixup USB HUB.\n");
+}
+
 int board_init(void)
 {
+	tusb8042a_swap_lines();
+
 	return 0;
 }
 
