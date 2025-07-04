2e159d7de1723934da043a1cc67f10e37fae73c0
Benjamin Hahn <B.Hahn@phytec.de>
2025-07-04 16:09:51 +0200

    bootstd: make it possible to use tftp for netboot with standardboot
    Add a function tftpb_run similar to dhcp_run, that calls the tftp
    command instead of the dhcp command and use tftp with standardboot
    instead of dhcp when ip_dyn environment variable is set to no. Otherwise
    continue to use dhcp as default.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Reviewed-by: Yannic Moog <y.moog@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- boot/bootmeth_script.c
+++ boot/bootmeth_script.c
@@ -129,7 +129,10 @@ static int script_read_bootflow_net(struct bootflow *bflow)
 	if (!fname)
 		return log_msg_ret("dhc", -EINVAL);
 
-	ret = dhcp_run(addr, fname, true);
+	if (env_get_yesno("ip_dyn") == 0)
+		ret = tftpb_run(addr, fname);
+	else
+		ret = dhcp_run(addr, fname, true);
 	if (ret)
 		return log_msg_ret("dhc", ret);
 
