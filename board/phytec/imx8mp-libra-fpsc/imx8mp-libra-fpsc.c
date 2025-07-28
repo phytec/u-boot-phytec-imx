phytec/u-boot-phytec-imx

commit b9dab710cdba73a17dae3012a5f1ef42c3039a20

BSPIMX8M-3634: WORKAROUND: board: phytec: imx8mp-libra-fpsc: Make USB Hub functional
The upstream ports of the USB Hub on Libra PCM-937-L 1618.0, 1618.1
have swapped pin polarity.
To workaround this we can set polarity in the USB Hub registers which
are available over i2c.

This will be fixed with 1618.2

Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
Reviewed-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/imx8mp-libra-fpsc/imx8mp-libra-fpsc.c
@@ -60,6 +60,29 @@ int dm_usb_gadget_handle_interrupts(struct udevice *dev)
 	return 0;
 }
 
+#define TUSB_PORT_POL_CRTL_REG	0xB
+#define TUSB_CUSTOM_POL		BIT(7)
+#define TUSB_P0_POL		BIT(0)
+
+/*
+ * WORKAROUND for PCM-937-L 1618.0, 1618.1.
+ * USB HUB TUSB8042A has swapped upstream pin polarity.
+ * Set i2c registers to inform the hub that the lines
+ * are swapped.
+ */
+void tusb8042a_swap_lines(void)
+{
+	const u8 pol_swap_val = (TUSB_CUSTOM_POL | TUSB_P0_POL);
+	const int addr = 0x44;
+	struct udevice *dev = 0;
+
+	int ret = i2c_get_chip_for_busnum(2, addr, 1, &dev);
+	if (!ret)
+		dm_i2c_write(dev, TUSB_PORT_POL_CRTL_REG, &pol_swap_val, 1);
+	else
+		printf("TUSB8042A: Failed to fixup USB HUB.\n");
+}
+
 static int setup_fec(void)
 {
 	struct iomuxc_gpr_base_regs *gpr =
@@ -77,6 +100,8 @@ int board_init(void)
 
 	if (ret)
 		printf("%s: EEPROM data init failed\n", __func__);
+
+	tusb8042a_swap_lines();
 
 	setup_fec();
 

commit fd2c0dd92e9092badba5907dd391bab00ef4eb4d

imx8mp-libra-fpsc: add i2c include
board code uses i2c functions but does not include i2c definitions.
Add i2c include.

Fixes: b9dab710cdba ("BSPIMX8M-3634: WORKAROUND: board: phytec: imx8mp-libra-fpsc: Make USB Hub functional")
Tested-by: Benjamin Hahn <B.Hahn@phytec.de>
Signed-off-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/imx8mp-libra-fpsc/imx8mp-libra-fpsc.c
@@ -11,6 +11,7 @@
 #include <dwc3-uboot.h>
 #include <env.h>
 #include <init.h>
+#include <i2c.h>
 #include <fdt_support.h>
 #include <jffs2/load_kernel.h>
 #include <miiphy.h>