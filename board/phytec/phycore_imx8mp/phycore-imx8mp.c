df3f97df778b150269c31091ade9abf5037ec644
Benjamin Hahn <B.Hahn@phytec.de>
2025-08-01 08:14:15 +0200

    board: phytec: phycore-imx8mp: Enable UUU support
    Enable UUU support for NXP BSP, as NXPs implementation of fastboot/UUU
    is different from upstream. With this patch also flashing eMMC and
    SD-Card over UUU works and the upper USB slot on the phyBOARD Pollux can
    be used for USB Sticks now as it can switch between USB Host and USB
    Gadget Mode.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    ported from 311e7967853 ("board: phytec: phycore-imx8mp: Enable UUU support")
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>

--- board/phytec/phycore_imx8mp/phycore-imx8mp.c
+++ board/phytec/phycore_imx8mp/phycore-imx8mp.c
@@ -8,16 +8,49 @@
 #include <asm/global_data.h>
 #include <asm/io.h>
 #include <asm/mach-imx/boot_mode.h>
+#include <asm/arch/clock.h>
 #include <env.h>
 #include <init.h>
 #include <fdt_support.h>
 #include <jffs2/load_kernel.h>
 #include <miiphy.h>
 #include <mtd_node.h>
+#include <usb.h>
+#include <dwc3-uboot.h>
 
 #include "../common/imx8m_som_detection.h"
 
 DECLARE_GLOBAL_DATA_PTR;
+
+static struct dwc3_device dwc3_device_data = {
+#ifdef CONFIG_SPL_BUILD
+	.maximum_speed = USB_SPEED_HIGH,
+#else
+	.maximum_speed = USB_SPEED_SUPER,
+#endif
+	.base = USB1_BASE_ADDR,
+	.dr_mode = USB_DR_MODE_PERIPHERAL,
+	.index = 0,
+	.power_down_scale = 2,
+};
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	if (index == 0 && init == USB_INIT_DEVICE) {
+		imx8m_usb_power(index, true);
+		return dwc3_uboot_init(&dwc3_device_data);
+	}
+	return 0;
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	if (index == 0 && init == USB_INIT_DEVICE) {
+		dwc3_uboot_exit(index);
+		imx8m_usb_power(index, false);
+	}
+	return 0;
+}
 
 #define EEPROM_ADDR		0x51
 #define EEPROM_ADDR_FALLBACK	0x59
@@ -57,6 +90,8 @@ int board_init(void)
 		printf("%s: EEPROM data init failed\n", __func__);
 
 	setup_fec();
+
+	init_usb_clk();
 
 	return 0;
 }
