8bf354edff280db5becc6409ba7cc9a55ca0d038
Benjamin Hahn <B.Hahn@phytec.de>
2026-01-21 11:58:51 +0100

    BACKPORT: bootstd: make it possible to use tftp for netboot with standardboot
    Add the option to load the bootscript with the tftp command (static IP)
    instead of the dhcp command (dynamic IP). For this a new function
    tftpb_run similar to dhcp_run, is needed. The selection of which command
    to use can be done with the ip_dyn environment variable, which can be
    set to yes or no. The ip_dyn variable was chosen as it is already in use
    on the imx platforms.
    Also edit the bootstd doc.
    
    (cherry picked from commit fae6c54d238279bf79c66ca65330425dff2c952f)
    Remote: https://source.denx.de/u-boot/u-boot.git
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- include/net-common.h
+++ include/net-common.h
@@ -490,6 +490,15 @@ int net_loop(enum proto_t protocol);
 int dhcp_run(ulong addr, const char *fname, bool autoload);
 
 /**
+ * tftpb_run() - Run TFTP on the current ethernet device
+ *
+ * @addr: Address to load the file into
+ * @fname: Filename of file to load (NULL to use the default filename)
+ * @return 0 if OK, -ENOENT if ant file was not found
+ */
+int tftpb_run(ulong addr, const char *fname);
+
+/**
  * do_tftpb - Run the tftpboot command
  *
  * @cmdtp: Command information for tftpboot
