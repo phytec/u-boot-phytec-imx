c93b73de82f3e4abdb405cae5c71deaf6af76f7e
Christoph Stoidner <c.stoidner@phytec.de>
2026-01-29 18:37:51 +0100

    board: phytec: phytec_som_detection: Prepare for phyFLEX i.MX93
    Prepare existing imx91/imx93 som detection for phyFLEX i.MX 93
    
    Signed-off-by: Christoph Stoidner <c.stoidner@phytec.de>
    Reviewed-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- board/phytec/common/imx91_93_som_detection.h
+++ board/phytec/common/imx91_93_som_detection.h
@@ -10,21 +10,37 @@
 
 #include "phytec_som_detection.h"
 
-#define PHYTEC_IMX91_93_SOM	77
+#define PHYCORE_IMX91_93_SOM	77
+#define PHYFLEX_IMX91_93_SOM	 5
 
-enum phytec_imx91_93_option_index {
-	PHYTEC_IMX91_93_OPT_DDR = 0,
-	PHYTEC_IMX91_93_OPT_EMMC = 1,
-	PHYTEC_IMX91_93_OPT_CPU = 2,
-	PHYTEC_IMX91_93_OPT_FREQ = 3,
-	PHYTEC_IMX91_93_OPT_NPU = 4,
-	PHYTEC_IMX91_93_OPT_DISP = 5,
-	PHYTEC_IMX91_93_OPT_ETH = 6,
-	PHYTEC_IMX91_93_OPT_FEAT = 7,
-	PHYTEC_IMX91_93_OPT_TEMP = 8,
-	PHYTEC_IMX91_93_OPT_BOOT = 9,
-	PHYTEC_IMX91_93_OPT_LED = 10,
-	PHYTEC_IMX91_93_OPT_EEPROM = 11,
+enum phytec_imx91_93_phycore_option_index {
+	PHYTEC_IMX91_93_PHYCORE_OPT_DDR = 0,
+	PHYTEC_IMX91_93_PHYCORE_OPT_EMMC = 1,
+	PHYTEC_IMX91_93_PHYCORE_OPT_CPU = 2,
+	PHYTEC_IMX91_93_PHYCORE_OPT_FREQ = 3,
+	PHYTEC_IMX91_93_PHYCORE_OPT_NPU = 4,
+	PHYTEC_IMX91_93_PHYCORE_OPT_DISP = 5,
+	PHYTEC_IMX91_93_PHYCORE_OPT_ETH = 6,
+	PHYTEC_IMX91_93_PHYCORE_OPT_FEAT = 7,
+	PHYTEC_IMX91_93_PHYCORE_OPT_TEMP = 8,
+	PHYTEC_IMX91_93_PHYCORE_OPT_BOOT = 9,
+	PHYTEC_IMX91_93_PHYCORE_OPT_LED = 10,
+	PHYTEC_IMX91_93_PHYCORE_OPT_EEPROM = 11,
+};
+
+enum phytec_imx91_93_phyflex_option_index {
+	PHYTEC_IMX91_93_PHYFLEX_OPT_DDR = 0,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_EMMC,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_CPU,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_FREQ,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_NPU,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_ETH,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_RTC_CLKSRC_TEMPSENS,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_TEMP,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_BOOTMODE,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_IOVOLTAGE,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_LED_EEPROMFACTORY_FLASHHIGHSPEED,
+	PHYTEC_IMX91_93_PHYFLEX_OPT_EEPROMUSER
 };
 
 enum phytec_imx91_93_voltage {
@@ -43,10 +59,20 @@ enum phytec_imx91_93_ddr_eeprom_code {
 	PHYTEC_IMX91_93_LPDDR4_2GB = 5,
 };
 
-u8 __maybe_unused phytec_imx91_93_detect(struct phytec_eeprom_data *data);
-u8 __maybe_unused phytec_imx91_93_get_opt(struct phytec_eeprom_data *data,
-					  enum phytec_imx91_93_option_index idx);
+u8 __maybe_unused phytec_imx91_93_detect(struct phytec_eeprom_data *data, u8 som_no);
+u8 __maybe_unused phytec_imx91_93_get_opt(struct phytec_eeprom_data *data, int idx);
 enum phytec_imx91_93_voltage __maybe_unused phytec_imx91_93_get_voltage
 	(struct phytec_eeprom_data *data);
 
+static inline u8 __maybe_unused phytec_imx91_93_phycore_get_opt(struct phytec_eeprom_data *data,
+		enum phytec_imx91_93_phycore_option_index idx)
+{
+	return phytec_imx91_93_get_opt(data, (int)idx);
+}
+
+static inline u8 __maybe_unused phytec_imx91_93_phyflex_get_opt(struct phytec_eeprom_data *data,
+		enum phytec_imx91_93_phyflex_option_index idx)
+{
+	return phytec_imx91_93_get_opt(data, (int)idx);
+}
 #endif /* _PHYTEC_IMX91_93_SOM_DETECTION_H */
