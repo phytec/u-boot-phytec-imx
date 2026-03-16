phytec/u-boot-phytec-imx

commit f5e9c4e8cb7485ddde04fa7ea0aff135733395e7

board: phytec: phycore-imx8mm: Update RAM Timings
Updated RAM timings with new generated settings using
DDR Tool v3.6.0_22-aa018543.

phyCORE-i.MX8 MM PCB Revision 7 PCB has an updated RAM interface
based on NXP recommendations. We need therefor a different set of
optimized timings for each revision. RAM Timing selection is done
via EEPROM data but can be also set during compile time.

This updated configuration uses only 1 frequency point for RAM
as the other frequency points are not used and therefor not
validated. It enables also recommended settings like DBI.

Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/phycore_imx8mm/spl.c
@@ -48,6 +48,7 @@ static void spl_dram_init(void)
 {
 	int ret;
 	enum phytec_imx8mm_ddr_eeprom_code size = PHYTEC_EEPROM_INVAL;
+	u8 rev = PHYTEC_EEPROM_INVAL;
 
 	ret = phytec_eeprom_data_setup_fallback(NULL, 0, EEPROM_ADDR,
 			EEPROM_ADDR_FALLBACK);
@@ -67,6 +68,22 @@ static void spl_dram_init(void)
 			size = PHYTEC_IMX8MM_DDR_4GB;
 	} else {
 		size = phytec_get_imx8m_ddr_size(NULL);
+	}
+
+	if (IS_ENABLED(CONFIG_PHYCORE_IMX8MM_RAM_STATIC_SOM_REV)) {
+		if (IS_ENABLED(CONFIG_PHYCORE_IMX8MM_USE_SOM_REV_6))
+			rev = 6;
+		else if (IS_ENABLED(CONFIG_PHYCORE_IMX8MM_USE_SOM_REV_7))
+			rev = 7;
+	} else {
+		rev = phytec_get_rev(NULL);
+	}
+
+	if (rev >= 7 || rev == PHYTEC_EEPROM_INVAL) {
+		debug("%s: Using rev7 RAM timings.\n", __func__);
+		set_dram_timings_rev7();
+	} else {
+		debug("%s: Using rev6 RAM timings.\n", __func__);
 	}
 
 	switch (size) {

board/phytec/phycore_imx8mm/Kconfig
@@ -41,5 +41,30 @@ config PHYCORE_IMX8MM_RAM_SIZE_4GB
 	  Set RAM size fix to 4GB for phyCORE-i.MX8MM.
 endchoice
 
+config PHYCORE_IMX8MM_RAM_STATIC_SOM_REV
+	bool "Set static phyCORE-i.MX8MM SoM revision for RAM timing setup"
+	default false
+	help
+	  phyCORE-i.MX8MM SoM revision 7 uses a updated set of optimized
+	  RAM timings compared to revision 6 and older. Set the SoM
+	  revision during compile time instead of being detected.
+
+choice
+	prompt "phyCORE-i.MX8MM RAM timing SoM revision static config"
+	depends on PHYCORE_IMX8MM_RAM_STATIC_SOM_REV
+	default PHYCORE_IMX8MM_USE_SOM_REV_7
+
+config PHYCORE_IMX8MM_USE_SOM_REV_6
+	bool "RAM Timings for SoM Revisions 6 and older"
+	help
+	  Use optimized RAM Timings for phyCORE-i.MX8MM SoM Revisions 6
+	  and older.
+
+config PHYCORE_IMX8MM_USE_SOM_REV_7
+	bool "RAM Timings for SoM Revision 7"
+	help
+	  Use optimized RAM Timings for phyCORE-i.MX8MM SoM Revision 7.
+endchoice
+
 source "board/phytec/common/Kconfig"
 endif

board/phytec/phycore_imx8mm/lpddr4_timing.h
@@ -8,5 +8,6 @@
 
 void set_dram_timings_1gb(void);
 void set_dram_timings_4gb(void);
+void set_dram_timings_rev7(void);
 
 #endif /* __LPDDR4_TIMING_H__ */

commit a2aeeb82e1d9955391d4fe182b6dc79719f28fd9

BSPIMX8M-3891: board: phytec: phycore-imx8mm: Set correct RAM timing fallback
Set Rev 7 timings as fallback if EEPROM data initialization fails.

Fixes: f5e9c4e8cb74 ("board: phytec: phycore-imx8mm: Update RAM Timings")

Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
Reviewed-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/phycore_imx8mm/spl.c
@@ -102,6 +102,7 @@ static void spl_dram_init(void)
 	return;
 out:
 	puts("Could not detect correct RAM size. Fall back to default.\n");
+	set_dram_timings_rev7();
 	ddr_init(&dram_timing);
 }
 