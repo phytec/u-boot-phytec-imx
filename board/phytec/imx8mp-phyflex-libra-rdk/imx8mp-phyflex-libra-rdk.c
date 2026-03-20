bfe74206427cbf4b565e39c02f38803673ff85ca
Benjamin Hahn <B.Hahn@phytec.de>
2026-03-20 11:24:20 +0100

    board: imx8mp-phyflex-libra-rdk: add USB PHY init to board_usb_init
    Add USB PHY init to board_usb_init to make USB-C port work as
    peripheral also when not booting via USB serial download. When booting
    via serial download, the USB PHY is already setup in U-Boot, but when
    booting from eMMC or SD card, the USB PHY is not setup and ums will
    fail with timeout error because of that.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>

--- board/phytec/imx8mp-phyflex-libra-rdk/imx8mp-phyflex-libra-rdk.c
+++ board/phytec/imx8mp-phyflex-libra-rdk/imx8mp-phyflex-libra-rdk.c
@@ -23,6 +23,28 @@
 
 #define EEPROM_ADDR		0x51
 
+#ifdef CONFIG_USB_DWC3
+
+#define USB_PHY_CTRL0		   0xF0040
+#define USB_PHY_CTRL0_REF_SSP_EN	BIT(2)
+
+#define USB_PHY_CTRL1		   0xF0044
+#define USB_PHY_CTRL1_RESET	     BIT(0)
+#define USB_PHY_CTRL1_COMMONONN	 BIT(1)
+#define USB_PHY_CTRL1_ATERESET	  BIT(3)
+#define USB_PHY_CTRL1_VDATSRCENB0       BIT(19)
+#define USB_PHY_CTRL1_VDATDETENB0       BIT(20)
+
+#define USB_PHY_CTRL2		   0xF0048
+#define USB_PHY_CTRL2_TXENABLEN0	BIT(8)
+
+#define USB_PHY_CTRL6		   0xF0058
+
+#define HSIO_GPR_BASE			       (0x32F10000U)
+#define HSIO_GPR_REG_0			      (HSIO_GPR_BASE)
+#define HSIO_GPR_REG_0_USB_CLOCK_MODULE_EN_SHIFT    (1)
+#define HSIO_GPR_REG_0_USB_CLOCK_MODULE_EN	  (0x1U << HSIO_GPR_REG_0_USB_CLOCK_MODULE_EN_SHIFT)
+
 static struct dwc3_device dwc3_device_data = {
 #ifdef CONFIG_SPL_BUILD
 	.maximum_speed = USB_SPEED_HIGH,
@@ -35,10 +57,49 @@ static struct dwc3_device dwc3_device_data = {
 	.power_down_scale = 2,
 };
 
+static void dwc3_nxp_usb_phy_init(struct dwc3_device *dwc3)
+{
+	u32 RegData;
+
+	/* enable usb clock via hsio gpr */
+	RegData = readl(HSIO_GPR_REG_0);
+	RegData |= HSIO_GPR_REG_0_USB_CLOCK_MODULE_EN;
+	writel(RegData, HSIO_GPR_REG_0);
+
+	/* USB3.0 PHY signal fsel for 100M ref */
+	RegData = readl(dwc3->base + USB_PHY_CTRL0);
+	RegData = (RegData & 0xfffff81f) | (0x2a << 5);
+	writel(RegData, dwc3->base + USB_PHY_CTRL0);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL6);
+	RegData &= ~0x1;
+	writel(RegData, dwc3->base + USB_PHY_CTRL6);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL1);
+	RegData &= ~(USB_PHY_CTRL1_VDATSRCENB0 | USB_PHY_CTRL1_VDATDETENB0 |
+			USB_PHY_CTRL1_COMMONONN);
+	RegData |= USB_PHY_CTRL1_RESET | USB_PHY_CTRL1_ATERESET;
+	writel(RegData, dwc3->base + USB_PHY_CTRL1);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL0);
+	RegData |= USB_PHY_CTRL0_REF_SSP_EN;
+	writel(RegData, dwc3->base + USB_PHY_CTRL0);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL2);
+	RegData |= USB_PHY_CTRL2_TXENABLEN0;
+	writel(RegData, dwc3->base + USB_PHY_CTRL2);
+
+	RegData = readl(dwc3->base + USB_PHY_CTRL1);
+	RegData &= ~(USB_PHY_CTRL1_RESET | USB_PHY_CTRL1_ATERESET);
+	writel(RegData, dwc3->base + USB_PHY_CTRL1);
+}
+#endif
+
 int board_usb_init(int index, enum usb_init_type init)
 {
 	if (index == 0 && init == USB_INIT_DEVICE) {
 		imx8m_usb_power(index, true);
+		dwc3_nxp_usb_phy_init(&dwc3_device_data);
 		return dwc3_uboot_init(&dwc3_device_data);
 	}
 	return 0;
