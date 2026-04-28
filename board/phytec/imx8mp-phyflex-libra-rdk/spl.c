phytec/u-boot-phytec-imx

commit d75afe53e7cbf3d1b2fc66badccfe91d7fb2af9b

board: phytec: imx8mp-phyflex-libra-rdk: Allow setting static RAM configuration
Add Kconfig entries to allow static size RAM configuration settings during
compile time.

Upstream-Status: Pending
Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
Reviewed-by: Yannic Moog <y.moog@phytec.de>
Tested-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/imx8mp-phyflex-libra-rdk/spl.c
@@ -42,7 +42,15 @@ void spl_dram_init(void)
 	if (!ret)
 		phytec_print_som_info(NULL);
 
-	size = phytec_get_imx8m_ddr_size(NULL);
+	if (IS_ENABLED(CONFIG_PHYFLEX_IMX8MP_RAM_STATIC_SIZE)) {
+		if (IS_ENABLED(CONFIG_PHYFLEX_IMX8MP_RAM_SIZE_2GB))
+			size = PHYTEC_PHYFLEX_IMX8MP_DDR_2GB;
+		else if (IS_ENABLED(CONFIG_PHYFLEX_IMX8MP_RAM_SIZE_4GB))
+			size = PHYTEC_PHYFLEX_IMX8MP_DDR_4GB;
+	} else {
+		size = phytec_get_imx8m_ddr_size(NULL);
+	}
+
 	switch (size) {
 	case PHYTEC_PHYFLEX_IMX8MP_DDR_2GB:
 		break;

board/phytec/imx8mp-phyflex-libra-rdk/Kconfig
@@ -12,5 +12,29 @@ config IMX_CONFIG
 config SYS_CONFIG_NAME
 	default "imx8mp-phyflex-libra-rdk"
 
+config PHYFLEX_IMX8MP_RAM_STATIC_SIZE
+	bool "Set phyFLEX-i.MX8MP RAM size static instead of detecting"
+	default false
+	help
+	  RAM size is automatic being detected with the help of
+	  the EEPROM introspection data. Set RAM size to a static value
+	  during compile time instead.
+
+choice
+	prompt "phyFLEX-i.MX8MP RAM size"
+	depends on PHYFLEX_IMX8MP_RAM_STATIC_SIZE
+	default PHYFLEX_IMX8MP_RAM_SIZE_2GB
+
+config PHYFLEX_IMX8MP_RAM_SIZE_2GB
+	bool "2GB RAM"
+	help
+	  Set RAM size static to 2GB for phyFLEX-i.MX8MP.
+
+config PHYFLEX_IMX8MP_RAM_SIZE_4GB
+	bool "4GB RAM"
+	help
+	  Set RAM size static to 4GB for phyFLEX-i.MX8MP.
+endchoice
+
 source "board/phytec/common/Kconfig"
 endif

commit 2739e5da44795b939c66440ce8fd2a5685bdbb12

board: phytec: imx8mp-phyflex-libra-rdk: Fix static RAM size
Setting RAM size during compile time should still work when EEPROM
detection fails. So make sure that the code does not fall back to
default RAM size early.

Fixes: d75afe53e7cb ("board: phytec: imx8mp-phyflex-libra-rdk: Allow setting static RAM configuration")

Upstream-Status: Pending
Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
Reviewed-by: Jan Remmet <j.remmet@phytec.de>
Signed-off-by: Christian Hemp <c.hemp@phytec.de>

board/phytec/imx8mp-phyflex-libra-rdk/spl.c
@@ -42,20 +42,20 @@ void spl_dram_init(void)
 	 * tree. Will be i2c1 (FPSC standard) in later boot stages.
 	 */
 	ret = phytec_eeprom_data_setup(NULL, 0, EEPROM_ADDR);
-	if (ret)
-		goto out;
+	if (!ret) {
+		ret = phytec_imx8m_detect(NULL);
+		if (!ret)
+			phytec_print_som_info(NULL);
 
-	ret = phytec_imx8m_detect(NULL);
-	if (!ret)
-		phytec_print_som_info(NULL);
+		size = phytec_get_imx8m_ddr_size(NULL);
+	}
 
+	/* Overwrite size with static configuration */
 	if (IS_ENABLED(CONFIG_PHYFLEX_IMX8MP_RAM_STATIC_SIZE)) {
 		if (IS_ENABLED(CONFIG_PHYFLEX_IMX8MP_RAM_SIZE_2GB))
 			size = PHYTEC_PHYFLEX_IMX8MP_DDR_2GB;
 		else if (IS_ENABLED(CONFIG_PHYFLEX_IMX8MP_RAM_SIZE_4GB))
 			size = PHYTEC_PHYFLEX_IMX8MP_DDR_4GB;
-	} else {
-		size = phytec_get_imx8m_ddr_size(NULL);
 	}
 
 	switch (size) {