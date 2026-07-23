3c4f3c45bd7119254a97c812f55a69f18f64968e
Leonard Anderweit <l.anderweit@phytec.de>
2026-07-23 14:24:04 +0200

    include: configs: imx95-phyflex-libra-rdk: Allow RAUC variables in secure context
    With secureboot enabled reading the environment from storage is
    generally disabled. Allow the required RAUC variables to be read.
    
    Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- include/configs/imx95-phyflex-libra-rdk.h
+++ include/configs/imx95-phyflex-libra-rdk.h
@@ -30,4 +30,9 @@
 /* Using ULP WDOG for reset */
 #define WDOG_BASE_ADDR          WDG3_BASE_ADDR
 
+#ifdef CONFIG_ENV_WRITEABLE_LIST
+/* Set environment flag validation to a list of env vars that must be writable */
+#define CFG_ENV_FLAGS_LIST_STATIC "BOOT_ORDER:sw,BOOT_system0_LEFT:dw,BOOT_system1_LEFT:dw"
+#endif
+
 #endif /* __IMX95_PHYFLEX_LIBRA_RDK_H */
